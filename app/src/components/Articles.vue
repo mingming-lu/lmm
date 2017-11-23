93d801b944268c2af72a152ba39f269c326c232a
akinaru-lu <akinaru.lu@gmail.com>
2017-11-23 22:21:21 +0900

    删除文章盒子中发布时间之前的posted time

--- app/src/components/Articles.vue
+++ app/src/components/Articles.vue
@@ -4,7 +4,7 @@
       <div class="lmm-card-4 lmm-margin lmm-white" style="width:66.6666%">
         <div class="lmm-container" style="text-align: left">
           <h2><b>{{ article.title }}</b></h2>
-          <p class="lmm-opacity">Posted time: {{ article.posted_time }}</p>
+          <p class="lmm-opacity">{{ article.posted_time }}</p>
           <p>{{ article.text }}</p>
           <p style="float: right"><button class="lmm-button lmm-padding-large lmm-white lmm-border"><b>READ MORE »</b></button></p>
         </div>
