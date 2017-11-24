cc67bf767fed717d030c8c4836c4bba0c66a2a17
akinaru-lu <akinaru.lu@gmail.com>
2017-11-25 00:17:17 +0900

    change time format

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -1,6 +1,21 @@
 export { formattedTime }
 
+var months = {
+  0: 'January',
+  1: 'February',
+  2: 'March',
+  3: 'April',
+  4: 'May',
+  5: 'June',
+  6: 'July',
+  7: 'August',
+  8: 'September',
+  9: 'October',
+  10: 'November',
+  11: 'December'
+}
+
 function formattedTime (timestamp) {
-  let date = new Date(timestamp * 1e3).toISOString().slice(0, 10)
-  return date.split('-').join('/')
+  let date = new Date(timestamp * 1e3)
+  return months[date.getUTCMonth()] + ' ' + date.getUTCDate() + ', ' + date.getUTCFullYear()
 }
