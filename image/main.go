109bc7b1ae8019a6c86b89a64789086beb807a6d
akinaru-lu <akinaru.lu@gmail.com>
2017-11-26 17:48:42 +0900

    add avatar to image service

--- image/main.go
+++ image/main.go
@@ -6,23 +6,30 @@ import (
     "os"
 )
 
-const ImageResPath = "image/res"
+const Res = "image/res"
+const Special = "image/special"
 
 func handleImage(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, ImageResPath + r.URL.Path)
+    http.ServeFile(w, r, Res + r.URL.Path)
 }
 
-func ensureRes() {
-    if _, err := os.Stat(ImageResPath); os.IsNotExist(err) {
-        os.Mkdir(ImageResPath, os.ModePerm)
+func avatar(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, Special + "/avatar.jpg")
+}
+
+func ensureDir(path string) {
+    if _, err := os.Stat(path); os.IsNotExist(err) {
+        os.Mkdir(path, os.ModePerm)
     }
 }
 
 func init() {
-    ensureRes()
+	ensureDir(Res)
+	ensureDir(Special)
 }
 
 func main() {
     http.HandleFunc("/", handleImage)
+    http.HandleFunc("/avatar", avatar)
     log.Fatal(http.ListenAndServe(":8082", nil))
 }
