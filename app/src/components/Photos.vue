f7a8db5621767e0c95040ddd11c176df3d9626f6
akinaru-lu <akinaru.lu@gmail.com>
2017-12-27 01:14:27 +0900

    delete unused value

--- app/src/components/Photos.vue
+++ app/src/components/Photos.vue
@@ -2,14 +2,14 @@
   <div class="lmm-row">
     <div class="lmm-left" style="width:50%">
       <div style="margin:8px">
-        <div v-for="(photo, index) in photos.slice(0, photos.length/2)" :key="photo.url" class="lmm-box">
+        <div v-for="photo in photos.slice(0, photos.length/2)" :key="photo.url" class="lmm-box">
           <img :src="photo.url" class="lmm-box">
         </div>
       </div>
     </div>
     <div class="lmm-right" style="width:50%">
       <div style="margin:8px">
-        <div v-for="(photo, index) in photos.slice(photos.length/2)" :key="photo.url" class="lmm-box">
+        <div v-for="photo in photos.slice(photos.length/2)" :key="photo.url" class="lmm-box">
           <img :src="photo.url" class="lmm-box">
         </div>
       </div>
