0be17d3fd74e3bb3f72358d936256d2e545ca8b1
akinaru-lu <akinaru.lu@gmail.com>
2017-12-28 04:21:01 +0900

    article db design and api design

--- api/articles/articles.go
+++ api/articles/articles.go
@@ -1,6 +1,7 @@
 package articles
 
 import (
+	"lmm/api/db"
 	"net/http"
 
 	"github.com/akinaru-lu/elesion"
@@ -15,23 +16,37 @@ type Result struct {
 }
 
 type Article struct {
-	Title      string `json:"title"`
-	Text       string `json:"text"`
-	PostedTime int    `json:"posted_time"`
-	EditedTime int    `json:"edited_time"`
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Text        string `json:"text"`
+	CreatedDate string `json:"created_date"`
+	EditedTime  string `json:"edited_date"`
 }
 
 func Handler(c *elesion.Context) {
-	data := Response{
-		Articles: []Article{
-			{Title: "怒斥香港记者", Text: "Too young too simple, sometimes naive.", PostedTime: 972749166},
-			{Title: "与华莱士谈笑风生", Text: "不知道比你们搞到哪里去了", PostedTime: 1142348400},
-			{Title: "视察国机二院", Text: "苟利国家生死以，岂因祸福避趋之", PostedTime: 1240488000},
-		},
+	articles, err := allArticlesByUserID(1)
+	if err != nil {
+		c.Status(http.StatusInternalServerError).Error(err.Error())
 	}
-	c.Status(http.StatusOK).JSON(data)
+	c.Status(http.StatusOK).JSON(articles)
 }
 
-func allArticles() []Article {
-	return nil
+func allArticlesByUserID(id int) ([]Article, error) {
+	d := db.New().Use("lmm")
+	defer d.Close()
+
+	itr, err := d.Query("SELECT id, title, text, created_date, edited_date FROM article WHERE user_id = ?", 1)
+	if err != nil {
+		return nil, err
+	}
+	defer itr.Close()
+
+	articles := make([]Article, 0)
+	for itr.Next() {
+		article := Article{}
+		itr.Scan(&article.ID, &article.Title, &article.Text, &article.CreatedDate, &article.EditedTime)
+
+		articles = append(articles, article)
+	}
+	return articles, nil
 }
