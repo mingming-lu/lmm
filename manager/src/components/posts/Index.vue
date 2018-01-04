9aead17065add924dbb2f2938fe341ef6a2eb614
Mingming Lu <akinaru-lu@gmail.com>
2018-01-04 19:28:16 +0900

    change manager post index layout

--- manager/src/components/posts/Index.vue
+++ manager/src/components/posts/Index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+
+    <!-- articles list -->
     <router-link to="/posts/new">post new article here</router-link>
-    <hr class="lmm-level-opacity">
     <h3>Articles List</h3>
     <div v-for="article in articles" :key="article.id"> 
       <router-link :to="'/posts/' + article.id + '/edit'">{{ article.title }}</router-link>
     </div>
+    <hr class="lmm-opacity">
+
   </div>
 </template>
 
