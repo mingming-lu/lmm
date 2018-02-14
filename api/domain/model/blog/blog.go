33c07b29e5845f64a271a33400e079957ab63030
akinaru-lu <akinaru.lu@gmail.com>
2018-02-15 02:58:54 +0900

    add domain model blog

---
+++ api/domain/model/blog/blog.go
@@ -0,0 +1,10 @@
+package blog
+
+type Blog struct {
+	ID        int64  `json:"id"`
+	User      int64  `json:"user"`
+	Title     string `json:"title"`
+	Text      string `json:"text"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
