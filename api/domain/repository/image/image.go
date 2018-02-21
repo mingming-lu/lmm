59f8c9ba0daef165c94aa14dcef87a6b78a09632
akinaru-lu <akinaru.lu@gmail.com>
2018-02-22 04:19:07 +0900

    complete add method in repository

--- api/domain/repository/image/image.go
+++ api/domain/repository/image/image.go
@@ -1,11 +1,40 @@
 package image
 
 import (
+	"io/ioutil"
+	"lmm/api/db"
 	model "lmm/api/domain/model/image"
+	"os"
 )
 
-func Add(userID int64, t model.ImageType, url string) error {
-	return nil
+const pathRaw = "image/raw/"
+
+func Add(userID int64, imageType model.ImageType, name string, data []byte) error {
+	d := db.Default()
+	defer d.Close()
+
+	stmt := d.Must("INSERT INTO image (user, type, url) VALUES (?, ?, ?)")
+	defer stmt.Close()
+
+	tx, err := d.Begin()
+	if err != nil {
+		return err
+	}
+	stmt = tx.Stmt(stmt)
+
+	_, err = stmt.Exec(userID, imageType, model.BaseURL+name)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = ioutil.WriteFile(pathRaw+name, data, os.ModePerm)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit()
 }
 
 func Fetch(userID int64, t model.ImageType) ([]model.Image, error) {
