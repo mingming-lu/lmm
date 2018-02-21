ea3159e08478d70779c8a1181a12162064e9059d
akinaru-lu <akinaru.lu@gmail.com>
2018-02-22 04:23:53 +0900

    controller for image

---
+++ api/controller/image/image.go
@@ -0,0 +1,50 @@
+package image
+
+import (
+	"io/ioutil"
+	usecase "lmm/api/usecase/image"
+	"lmm/api/usecase/user"
+	"net/http"
+
+	"github.com/akinaru-lu/elesion"
+)
+
+func Upload(c *elesion.Context) {
+	usr, err := user.Verify(c.Request.Header.Get("Authorization"))
+	if err != nil {
+		c.Status(http.StatusUnauthorized).String("Unauthorized, invalid token").Error(err.Error())
+		return
+	}
+
+	file, handler, err := c.Request.FormFile("src")
+	if err != nil {
+		c.Status(http.StatusBadRequest).String("Invalid image").Error(err.Error())
+		return
+	}
+	defer file.Close()
+
+	contentType := handler.Header.Get("Content-Type")
+	switch contentType {
+	case "image/jpeg", "image/png":
+	default:
+		c.Status(http.StatusBadRequest).String("Invalid content type: " + contentType)
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		c.Status(http.StatusBadRequest).String("Invalid input image").Error(err.Error())
+		return
+	}
+
+	err = usecase.Upload(usr.ID, c.Request.FormValue("type"), data)
+	if err != nil {
+		c.Status(http.StatusInternalServerError).String("Internal server error").Error(err.Error())
+		return
+	}
+	c.Status(http.StatusCreated).String("success")
+}
+
+func Get(c *elesion.Context) {
+	c.Status(http.StatusNotImplemented).String("Not implemented")
+}
