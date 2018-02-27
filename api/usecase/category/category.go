1bc57c3d62f832c43268d1f46ced962aedeb9f88
akinaru-lu <akinaru.lu@gmail.com>
2018-02-28 06:18:19 +0900

    change category usecase

--- api/usecase/category/category.go
+++ api/usecase/category/category.go
@@ -5,18 +5,14 @@ import (
 	repo "lmm/api/domain/repository/category"
 )
 
-func Register(userID, blogID int64, name string) (int64, error) {
-	return repo.Add(userID, blogID, name)
+func Register(userID int64, name string) (int64, error) {
+	return repo.Add(userID, name)
 }
 
-func Update(userID, blogID int64, name string) error {
-	return repo.Update(userID, blogID, name)
+func Update(userID, categoryID int64, name string) error {
+	return repo.Update(userID, categoryID, name)
 }
 
-func FetchByUser(userID int64) ([]model.Category, error) {
+func FetchByUser(userID int64) ([]model.Minimal, error) {
 	return repo.ByUser(userID)
-}
-
-func FetchByBlog(blogID int64) (*model.Category, error) {
-	return repo.ByBlog(blogID)
 }
