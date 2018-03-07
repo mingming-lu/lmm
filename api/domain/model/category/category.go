b0f3b8e1c9fc78b55bc5bf9b278b11848b47e706
akinaru-lu <akinaru.lu@gmail.com>
2018-03-08 00:57:47 +0900

    build category model from reader

--- api/domain/model/category/category.go
+++ api/domain/model/category/category.go
@@ -1,11 +1,20 @@
 package category
 
-type Minimal struct {
-	Name string `json:"name"`
-}
+import (
+	"encoding/json"
+	"io"
+)
 
 type Category struct {
 	ID   int64  `json:"id"`
 	User int64  `json:"user"`
 	Name string `json:"name"`
+}
+
+func Read(reader io.Reader) (*Category, error) {
+	model := Category{}
+	if err := json.NewDecoder(reader).Decode(&model); err != nil {
+		return nil, err
+	}
+	return &model, nil
 }
