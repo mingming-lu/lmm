eb1ec4d2e3907ae328e9c63dff3fc82c323354a3
akinaru-lu <akinaru.lu@gmail.com>
2018-03-09 07:11:49 +0900

    design home page

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="center">
-    <h1 class="sub">一个一点也不萌的网站</h1>
-    <h1>卢明鸣的博客</h1>
+
+    <div class="bg bgimg-1">
+      <div class="container display-middle" style="white-space:nowrap;">
+        <h1 class="center">一个一点也不萌的网站</h1>
+        <hr class="opacity-plus">
+        <p class=""></p>
+        <p class="center">没逼你看，爱看看不看滚</p>
+        <p class="center">就是这么Dior，不服来咬我啊</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
+  created () {
+    document.getElementsByTagName('header')[0].className = 'fixed'
+  },
+  destroyed () {
+    document.getElementsByTagName('header')[0].className = 'white sticky header-shadow'
+  }
 }
 </script>
 
 <style scoped>
-.sub {
-  zoom: 0.8;
-}
-div {
-  margin: 128px auto !important;
-}
 h1 {
+  font-weight: normal;
   letter-spacing: 6px;
+}
+.bg {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.bgimg-1 {
+  background-image: url('/static/img/bg.png');
+  min-height: 100vh;
+}
+.display-middle{
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  -ms-transform:translate(-50%,-50%);
 }
 </style>
