564eeb6f80723e1c0dd78de91a432f374d1b693c
akinaru-lu <akinaru.lu@gmail.com>
2018-05-02 00:28:37 +0900

    footer vue file

--- app/src/components/common/Footer.vue
+++ app/src/components/common/Footer.vue
@@ -1,23 +1,27 @@
 <template>
   <footer class="center">
-    <hr class="opacity-plus">
+    <hr>
     <a href="https://github.com/akinaru-lu" target="_blank">
-      <i class="fa fa-github link white"></i>
+      <i class="fa fa-github link"></i>
     </a>
     <a href="https://www.linkedin.com/in/lumingming/" target="_blank">
-      <i class="fa fa-linkedin link white"></i>
+      <i class="fa fa-linkedin link"></i>
     </a>
-    <p>&copy; 2018 <router-link to="/home#author" class="link white">
+    <p>&copy; 2018 <router-link to="/home#author" class="link">
       <u>Lu Mingming</u>
     </router-link></p>
   </footer>
 </template>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/styles.scss';
 footer {
-  width: 100%;
-  position: fixed !important;
-  z-index: -99 !important;
+  hr {
+    opacity: 0.15;
+  }
   bottom: 0 !important;
+  position: fixed !important;
+  width: 100%;
+  z-index: -99 !important;
 }
 </style>
