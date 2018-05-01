0eb4eb384e0e2b4ae08dc6fe8dc74bf3035a92fc
Mingming Lu <akinaru.lu@gmail.com>
2018-05-01 18:21:55 +0900

    separated footer page

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <Header />
     <router-view style="margin-bottom:86px;" />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
