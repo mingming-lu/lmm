852d34d7ddc31ee2a3b9e892d4a335f1b73c6cc4
akinaru-lu <akinaru.lu@gmail.com>
2018-05-02 21:42:08 +0900

    照片页面可根据宽度进行瀑布流或普通的垂直列表渲染

--- app/src/components/Photos.vue
+++ app/src/components/Photos.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container">
-    <div class="left">
-      <img v-for="photo in left" :key="photo.name" :src="url(photo.name)">
+
+    <div v-if="wideMode">
+      <div class="left">
+        <img v-for="photo in left" :key="photo.name" :src="url(photo.name)">
+      </div>
+      <div class="right">
+        <img v-for="photo in right" :key="photo.name" :src="url(photo.name)">
+      </div>
     </div>
-    <div class="right">
-      <img v-for="photo in right" :key="photo.name" :src="url(photo.name)">
-    </div>
+
+    <img v-if="!wideMode" v-for="photo in photos" :key="photo.name" :src="url(photo.name)">
   </div>
 </template>
 
@@ -14,26 +19,37 @@ import axios from 'axios'
 export default {
   data () {
     return {
+      wideMode: false,
       left: [],
-      right: []
+      right: [],
+      photos: []
     }
   },
   created () {
+    this.calcIsWideMode()
+    window.addEventListener('resize', this.calcIsWideMode)
     axios.get('https://api.lmm.im/v1/users/1/images/photos').then((res) => {
-      res.data.forEach((image, index) => {
+      this.photos.push(...res.data)
+      res.data.forEach((photo, index) => {
         if (index % 2 === 0) {
-          this.left.push(image)
+          this.left.push(photo)
         } else {
-          this.right.push(image)
+          this.right.push(photo)
         }
       })
     }).catch((e) => {
       console.log(e)
     })
   },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.calcIsWideMode)
+  },
   methods: {
     url: (name) => {
       return 'https://image.lmm.im/' + name
+    },
+    calcIsWideMode () {
+      this.wideMode = window.innerWidth > 800
     }
   }
 }
