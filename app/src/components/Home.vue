44f255da303d322e109b76a59d1085a70b62d358
akinaru-lu <akinaru.lu@gmail.com>
2018-05-03 01:46:41 +0900

    主页选定第一张背景图片

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <h1 class="center title">一个一点也不萌的网站</h1>
-    <h1 class="center">卢明鸣的博客</h1>
+    <div class="background-image-1" />
   </div>
 </template>
 
@@ -12,11 +11,15 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/scss/styles.scss';
-h1 {
-  font-weight: 500;
-  letter-spacing: 6px;
+.background-image {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
-.title {
-  margin-top: 128px;
+.background-image-1 {
+  @extend .background-image;
+  background-image: url('/static/img/home-bg1.png');
+  min-height: 100vh;
 }
 </style>
