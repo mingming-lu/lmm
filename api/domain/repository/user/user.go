49c389944edb7cfe3d254514141c3fa7369c42ae
akinaru-lu <akinaru.lu@gmail.com>
2018-05-12 04:32:53 +0900

    refactor of repo

--- api/domain/repository/user/user.go
+++ api/domain/repository/user/user.go
@@ -5,6 +5,34 @@ import (
 	model "lmm/api/domain/model/user"
 )
 
+type Repository struct {
+	db func() *db.DB
+}
+
+func New() *Repository {
+	return &Repository{db: func() *db.DB { return db.Default() }}
+}
+
+// Save return a User model with generated id
+func (repo *Repository) Save(user *model.User) (*model.User, error) {
+	db := repo.db()
+	defer db.Close()
+
+	stmt := db.Must(`INSERT INTO user (name, password, guid, token, created_at) VALUES (?, ?, ?, ?, ?)`)
+	defer stmt.Close()
+
+	res, err := stmt.Exec(user.Name, user.Password, user.GUID, user.Token, user.CreatedAt.UTC())
+	if err != nil {
+		return nil, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	user.ID = uint64(id)
+	return user, nil
+}
+
 func Add(name, password, guid, token string) (int64, error) {
 	d := db.Default()
 	defer d.Close()
