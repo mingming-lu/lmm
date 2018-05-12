be3d5fd6a07ad61d1fb0e9778637802bb260569c
akinaru-lu <akinaru.lu@gmail.com>
2018-05-13 01:35:32 +0900

    test for duplicate user name

--- api/usecase/user/user_test.go
+++ api/usecase/user/user_test.go
@@ -16,3 +16,15 @@ func TestSignUp(t *testing.T) {
 	tester.NoError(err)
 	tester.Is(uint64(1), id)
 }
+
+func TestSignUp_Duplicate(t *testing.T) {
+	testing.InitTable("user")
+	tester := testing.NewTester(t)
+
+	auth := Auth{Name: "foobar", Password: "1234"}
+	New(repo.New()).SignUp(testing.StructToRequestBody(auth))
+	id, err := New(repo.New()).SignUp(testing.StructToRequestBody(auth))
+	tester.Error(err)
+	tester.Is(ErrDuplicateUserName, err)
+	tester.Is(uint64(0), id)
+}
