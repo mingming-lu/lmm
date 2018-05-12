22a6a55815d37942c77323a139ab0cfbedddeb99
akinaru-lu <akinaru.lu@gmail.com>
2018-05-12 22:58:55 +0900

    wrapper of http.Request for test

--- api/testing/http.go
+++ api/testing/http.go
@@ -1,9 +1,35 @@
 package testing
 
 import (
+	"io"
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 )
+
+type Request struct {
+	*http.Request
+}
+
+func NewRequest(method, path string, body io.Reader) *Request {
+	return &Request{Request: httptest.NewRequest(method, path, body)}
+}
+
+func GET(path string) *Request {
+	return NewRequest(http.MethodGet, path, nil)
+}
+
+func POST(path string, body io.Reader) *Request {
+	return NewRequest(http.MethodPost, path, body)
+}
+
+func PUT(path string, body io.Reader) *Request {
+	return NewRequest(http.MethodPut, path, body)
+}
+
+func DELETE(path string, body io.Reader) *Request {
+	return NewRequest(http.MethodDelete, path, body)
+}
 
 type Response struct {
 	*httptest.ResponseRecorder
