f300971328f579292d349e7f31ea39d5fb1dc6cc
akinaru-lu <akinaru.lu@gmail.com>
2018-05-16 04:33:45 +0900

    return resonable status according to error returned from usecase

--- api/context/account/ui/signin.go
+++ api/context/account/ui/signin.go
@@ -18,16 +18,18 @@ func SignIn(c *elesion.Context) {
 	}
 
 	user, err := usecase.New(repository.New()).SignIn(auth.Name, auth.Password)
-	if err != nil {
+	switch err {
+	case nil:
+		c.Status(http.StatusOK).JSON(SignInResponse{
+			ID:    user.ID,
+			Name:  user.Name,
+			Token: user.Token,
+		})
+	case usecase.ErrEmptyUserNameOrPassword:
+		c.Status(http.StatusBadRequest).String(err.Error())
+	case usecase.ErrInvalidUserNameOrPassword:
 		c.Status(http.StatusNotFound).String(err.Error())
-		return
+	default:
+		c.Status(http.StatusInternalServerError).String(http.StatusText(http.StatusInternalServerError)).Error(err.Error())
 	}
-
-	response := SignInResponse{
-		ID:    user.ID,
-		Name:  user.Name,
-		Token: user.Token,
-	}
-
-	c.Status(http.StatusOK).JSON(response)
 }
