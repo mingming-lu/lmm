deec15b4948982ad76a3a40029c02ed96030b19d
akinaru-lu <akinaru.lu@gmail.com>
2018-05-16 22:40:53 +0900

    using test service

--- api/context/account/usecase/signup_test.go
+++ api/context/account/usecase/signup_test.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"lmm/api/context/account/domain/repository"
+	testingService "lmm/api/context/account/domain/service/testing"
 	"lmm/api/testing"
 )
 
@@ -19,11 +20,11 @@ func TestSignUp_Duplicate(t *testing.T) {
 	testing.InitTable("user")
 	tester := testing.NewTester(t)
 
-	auth := Auth{Name: "foobar", Password: "1234"}
+	user := testingService.NewUser()
+
 	repo := repository.New()
-	New(repo).SignUp(auth.Name, auth.Password)
-	id, err := New(repo).SignUp(auth.Name, auth.Password)
+	id, err := New(repo).SignUp(user.Name, user.Password)
 	tester.Error(err)
-	tester.Is(ErrDuplicateUserName, err)
+	tester.Is(ErrDuplicateUserName.Error(), err.Error())
 	tester.Is(uint64(0), id)
 }
