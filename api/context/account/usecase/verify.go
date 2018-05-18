aeba6b3c4013c7fbcaf4c02fd8714fffdbbca487
Mingming Lu <akinaru.lu@gmail.com>
2018-05-18 21:27:03 +0900

    handle usecase error msg

--- api/context/account/usecase/verify.go
+++ api/context/account/usecase/verify.go
@@ -4,21 +4,17 @@ import (
 	"lmm/api/context/account/domain/model"
 	"lmm/api/context/account/domain/repository"
 	"lmm/api/context/account/domain/service"
-	"log"
 )
 
 func VerifyToken(encodedToken string) (user *model.User, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Printf("%v", r)
-			err = ErrInvalidToken
-		}
-	}()
 	token, err := service.DecodeToken(encodedToken)
 	if err != nil {
-		return nil, err
+		return nil, ErrInvalidToken
 	}
 
 	user, err = repository.New().FindByToken(token)
-	return user, err
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+	return user, nil
 }
