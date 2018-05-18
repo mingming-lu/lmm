aa12b95d12a7d66aa990788dfbb9db69caff8340
Mingming Lu <akinaru.lu@gmail.com>
2018-05-18 21:27:36 +0900

    more tests fot verify token

--- api/context/account/usecase/verify_test.go
+++ api/context/account/usecase/verify_test.go
@@ -1,13 +1,13 @@
 package usecase
 
 import (
-	"lmm/api/context/account/domain/model"
 	"lmm/api/context/account/domain/service"
 	testingService "lmm/api/context/account/domain/service/testing"
 	"lmm/api/testing"
 )
 
 func TestVerify_Success(t *testing.T) {
+	testing.InitTable("user")
 	tester := testing.NewTester(t)
 
 	origin := testingService.NewUser()
@@ -21,11 +21,7 @@ func TestVerify_Success(t *testing.T) {
 func TestVerify_InvalidToken(t *testing.T) {
 	tester := testing.NewTester(t)
 
-	var user *model.User
-	var err error
-	tester.Output("Failed to parse base64 encoded token:", func() {
-		user, err = VerifyToken("invalid")
-	})
+	user, err := VerifyToken("invalid")
 	tester.Error(ErrInvalidToken, err)
 	tester.Nil(user)
 }
