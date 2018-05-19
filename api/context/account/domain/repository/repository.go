441461758db4251f41021a6d364a524e1a087dbe
Mingming Lu <akinaru.lu@gmail.com>
2018-05-19 09:01:42 +0900

    delete unused methods

--- api/context/account/domain/repository/repository.go
+++ api/context/account/domain/repository/repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"lmm/api/context/account/domain/model"
-	"lmm/api/db"
 	"lmm/api/domain/repository"
 
 	"github.com/akinaru-lu/errors"
@@ -72,34 +71,4 @@ func (repo *repo) FindByToken(token string) (*model.User, error) {
 		return nil, errors.New(err.Error())
 	}
 	return &user, nil
-}
-
-func Add(name, password, guid, token string) (int64, error) {
-	d := db.Default()
-	defer d.Close()
-
-	stmt := d.Must(`INSERT INTO user (name, password, guid, token) values (?, ?, ?, ?)`)
-	defer stmt.Close()
-
-	res, err := stmt.Exec(name, password, guid, token)
-	if err != nil {
-		return 0, err
-	}
-
-	return res.LastInsertId()
-}
-
-func ByToken(token string) (*model.User, error) {
-	d := db.Default()
-	defer d.Close()
-
-	stmt := d.Must("SELECT id, name, password, guid, token, created_at FROM user WHERE token = ?")
-	defer stmt.Close()
-
-	user := model.User{}
-	err := stmt.QueryRow(token).Scan(
-		&user.ID, &user.Name, &user.Password, &user.GUID, &user.Token, &user.CreatedAt,
-	)
-
-	return &user, err
 }
