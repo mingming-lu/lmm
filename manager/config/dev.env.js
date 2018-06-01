5d74688ec63b77d5840559e2f1da99acf70bde8c
akinaru-lu <akinaru.lu@gmail.com>
2018-06-02 05:11:11 +0900

    setting urls for different mode respectly

--- manager/config/dev.env.js
+++ manager/config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_URL_BASE: '"http://api.lmm.local"',
+  IMAGE_URL_BASE: '"http://image.lmm.local"'
 })
