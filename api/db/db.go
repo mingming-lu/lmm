093391a553ab2f8e1324eb71b22646ca7e937117
akinaru-lu <akinaru.lu@gmail.com>
2018-06-07 03:34:49 +0900

    refactoring db package

--- api/db/db.go
+++ api/db/db.go
@@ -3,10 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	"net/url"
 	"os"
-	"strings"
-	"sync"
 
 	"github.com/akinaru-lu/errors"
 	_ "github.com/go-sql-driver/mysql"
@@ -24,41 +21,9 @@ var (
 	connParams = "parseTime=true"
 	dbSrcName  = "root:@tcp(lmm-mysql:3306)/"
 )
-var mux sync.Mutex
-
-func SetDefaultDatabaseName(name string) {
-	mux.Lock()
-	dbName = name
-	mux.Unlock()
-}
 
 type DB struct {
 	*sql.DB
-}
-
-type Values map[string]interface{}
-
-func NewValues() Values {
-	return make(Values)
-}
-
-func NewValuesFromURL(values url.Values) Values {
-	ret := NewValues()
-	for k := range values {
-		ret[k] = values.Get(k)
-	}
-	return ret
-}
-
-func (values Values) Where() string {
-	s := ""
-	for k, v := range values {
-		s += fmt.Sprintf(`%s="%v" AND `, k, v)
-	}
-	if s != "" {
-		s = "WHERE " + strings.TrimSuffix(s, " AND ")
-	}
-	return s
 }
 
 func New() *DB {
@@ -69,40 +34,7 @@ func New() *DB {
 	return &DB{DB: conn}
 }
 
-// Deprecated: select database on creating db connection
-func Default() *DB {
-	// if dbName == "" {
-	// 	panic("Default database has not been set")
-	// }
-	return New()
-}
-
-func (db *DB) CreateDatabase(name string) *DB {
-	_, err := db.Exec("CREATE DATABASE IF NOT EXISTS " + name)
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-func (db *DB) DropDatabase(name string) *DB {
-	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-// Deprecated: select database on creating db connection instead of use
-func (db *DB) Use(database string) *DB {
-	_, err := db.Exec("USE " + database)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-func (db *DB) Must(query string) *sql.Stmt {
+func (db *DB) MustPrepare(query string) *sql.Stmt {
 	stmt, err := db.Prepare(query)
 	if err != nil {
 		panic(err)
@@ -110,18 +42,7 @@ func (db *DB) Must(query string) *sql.Stmt {
 	return stmt
 }
 
-func (db *DB) Mustf(format string, args ...interface{}) *sql.Stmt {
+func (db *DB) MustPreparef(format string, args ...interface{}) *sql.Stmt {
 	query := fmt.Sprintf(format, args...)
-	return db.Must(query)
-}
-
-func (db *DB) Exists(query string, args ...interface{}) (bool, error) {
-	query = fmt.Sprintf("SELECT EXISTS (%s)", query)
-
-	var exists bool
-	err := db.QueryRow(query, args...).Scan(&exists)
-	if err != nil && err != sql.ErrNoRows {
-		return false, err
-	}
-	return exists, nil
+	return db.MustPrepare(query)
 }
