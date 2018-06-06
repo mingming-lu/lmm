ad53fa98ded976e220aa5ed529beaf746e5ff18d
akinaru-lu <akinaru.lu@gmail.com>
2018-06-07 02:59:17 +0900

    auth use http

--- api/usecase/auth/bearer.go
+++ api/usecase/auth/bearer.go
@@ -3,31 +3,29 @@ package auth
 import (
 	accountService "lmm/api/context/account/appservice"
 	accountRepository "lmm/api/context/account/domain/repository"
-	"net/http"
+	"lmm/api/http"
 	"regexp"
-
-	"github.com/akinaru-lu/elesion"
 )
 
 var (
 	PatternBearerAuthorization = regexp.MustCompile(`^Bearer (.+)$`)
 )
 
-func BearerAuth(handler elesion.Handler) elesion.Handler {
-	return func(c *elesion.Context) {
+func BearerAuth(handler http.Handler) http.Handler {
+	return func(c *http.Context) {
 		auth := c.Request.Header.Get("Authorization")
 		mathed := PatternBearerAuthorization.FindStringSubmatch(auth)
 		if len(mathed) != 2 {
-			c.Status(http.StatusUnauthorized).String(http.StatusText(http.StatusUnauthorized))
+			http.Unauthorized(c)
 			return
 		}
 		token := mathed[1]
 		user, err := accountService.New(accountRepository.New()).VerifyToken(token)
 		if err != nil {
-			c.Status(http.StatusUnauthorized).String(http.StatusText(http.StatusUnauthorized))
+			http.Unauthorized(c)
 			return
 		}
-		c.Fields().Set("user", user)
+		c.Values().Set("user", user)
 		handler(c)
 	}
 }
