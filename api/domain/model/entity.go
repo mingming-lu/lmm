b6e27f22cde8f1567b2fa51317ed1a333373e37f
Mingming Lu <akinaru.lu@gmail.com>
2018-06-07 20:39:50 +0900

    base domain entity model

--- api/domain/model/entity.go
+++ api/domain/model/entity.go
@@ -1,13 +1,5 @@
 package model
 
-type Entity struct {
-	id uint64
-}
-
-func (e *Entity) ID() uint64 {
-	return e.id
-}
-
-func NewEntity(id uint64) *Entity {
-	return &Entity{id: id}
+type Entity interface {
+	ID() uint64
 }
