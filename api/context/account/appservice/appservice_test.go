5f8f731aaa6ca6c53b4197ff0ae2ffa37b13abc7
akinaru-lu <akinaru.lu@gmail.com>
2018-06-08 03:53:06 +0900

    util method for testing

--- api/context/account/appservice/appservice_test.go
+++ api/context/account/appservice/appservice_test.go
@@ -1,8 +1,15 @@
 package appservice
 
-import "lmm/api/testing"
+import (
+	"lmm/api/testing"
+	"lmm/api/utils/uuid"
+)
 
 func Main(m *testing.M) {
 	testing.InitTableAll()
 	m.Run()
+}
+
+func randomUserNameAndPassword() (string, string) {
+	return uuid.New()[:32], uuid.New()
 }
