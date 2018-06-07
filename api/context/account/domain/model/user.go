2173614d3338fde543821ae76d43a4bef2365eac
Mingming Lu <akinaru.lu@gmail.com>
2018-06-07 20:40:43 +0900

    user struct hold private member variables

--- api/context/account/domain/model/user.go
+++ api/context/account/domain/model/user.go
@@ -2,45 +2,61 @@ package model
 
 import (
 	"lmm/api/domain/model"
-	"lmm/api/utils/sha256"
-	"lmm/api/utils/uuid"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-type Minimal struct {
-	Name     string `json:"name"`
-	Password string `json:"password"`
-}
-
-type Response struct {
-	ID    uint64 `json:"id"`
-	Name  string `json:"name"`
-	Token string `json:"token"`
-}
-
-// User model is a Entry
 type User struct {
 	model.Entity
-	Name      string
-	Password  string
-	GUID      string
-	Token     string
-	CreatedAt time.Time
+	id        uint64
+	name      string
+	password  string
+	guid      string
+	token     string
+	createdAt time.Time
 }
 
-// New new a User model which is going to be saved to repository (User.ID is 0!!)
-// If want to build a User model from existing data, just do this -> user := &User{ID: xxx, ...}
-// TODO New => NewUser
-func NewUser(name, password string) *User {
-	token := uuid.New()
-	guid := uuid.New()
-	encodedPassword := sha256.Hex([]byte(guid + password)) // digest
-
+func NewUser(id uint64, name, password, guid, token string, createdAt time.Time) *User {
 	return &User{
-		Name:      name,
-		Password:  encodedPassword,
-		GUID:      guid,
-		Token:     token,
-		CreatedAt: time.Now(),
+		id:        id,
+		name:      name,
+		password:  password,
+		guid:      guid,
+		token:     token,
+		createdAt: createdAt,
 	}
+}
+
+func (u *User) ID() uint64 {
+	return u.id
+}
+
+func (u *User) Name() string {
+	return u.name
+}
+
+func (u *User) Password() string {
+	return u.password
+}
+
+func (u *User) UpdatePassword(password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) GUID() string {
+	return u.guid
+}
+
+func (u *User) Token() string {
+	return u.token
+}
+
+func (u *User) CreatedAt() time.Time {
+	return u.createdAt
 }
