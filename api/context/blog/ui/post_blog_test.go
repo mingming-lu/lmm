cc1e9f0b3db9649157e8d505a4c794d8c91910a2
akinaru-lu <akinaru.lu@gmail.com>
2018-06-09 03:59:34 +0900

    testing

---
+++ api/context/blog/ui/post_blog_test.go
@@ -0,0 +1,40 @@
+package ui
+
+import (
+	"io"
+	"lmm/api/http"
+	"lmm/api/testing"
+	"lmm/api/usecase/auth"
+)
+
+func TestPostBlog_Success(t *testing.T) {
+	tester := testing.NewTester(t)
+
+	headers := make(map[string]string)
+	headers["Authorization"] = "Bearer " + user.Token()
+
+	blog := Blog{
+		Title: "blog title",
+		Text:  "blog text",
+	}
+
+	res := postBlog(headers, testing.StructToRequestBody(blog))
+	tester.Is(http.StatusCreated, res.StatusCode())
+}
+
+func postBlog(headers map[string]string, requestBody io.Reader) *testing.Response {
+	request := testing.POST("/v1/blog", requestBody)
+	if headers != nil {
+		for k, v := range headers {
+			request.Header.Add(k, v)
+		}
+	}
+
+	router := testing.NewRouter()
+	router.POST("/v1/blog", auth.BearerAuth(PostBlog))
+
+	res := testing.NewResponse()
+	router.ServeHTTP(res, request)
+
+	return res
+}
