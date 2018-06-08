8d3ad7fed1aa3a10d2ff01b6cfd3e0e99204b2f2
akinaru-lu <akinaru.lu@gmail.com>
2018-06-09 04:14:44 +0900

    testing domain repository

---
+++ api/context/blog/domain/repository/blog_test.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	accountFactory "lmm/api/context/account/domain/factory"
+	"lmm/api/context/blog/domain/factory"
+	"lmm/api/testing"
+	"lmm/api/utils/uuid"
+)
+
+func TestAddBlog(t *testing.T) {
+	tester := testing.NewTester(t)
+	repo := NewBlogRepository()
+
+	name, password := uuid.New()[:32], uuid.New()
+	title, text := uuid.New(), uuid.New()
+	user, _ := accountFactory.NewUser(name, password)
+	blog, _ := factory.NewBlog(user.ID(), title, text)
+	err := repo.Add(blog)
+
+	tester.NoError(err)
+
+	var (
+		userID    uint64
+		blogTitle string
+		blogText  string
+	)
+	db := repo.DB()
+	defer db.Close()
+
+	stmt := db.MustPrepare(`SELECT user, title, text FROM blog WHERE id = ?`)
+	defer stmt.Close()
+
+	stmt.QueryRow(blog.ID()).Scan(&userID, &blogTitle, &blogText)
+
+	tester.Is(user.ID(), userID)
+	tester.Is(blog.Title(), blogTitle)
+	tester.Is(blog.Text(), blogText)
+}
