9bc1bbfd720a87b2b1d7f2bbd6fd78b30f2739f6
akinaru-lu <akinaru.lu@gmail.com>
2018-06-09 03:58:46 +0900

    bug fix

--- api/context/blog/ui/ui.go
+++ api/context/blog/ui/ui.go
@@ -1,17 +1,16 @@
 package ui
 
 import (
+	"fmt"
 	account "lmm/api/context/account/domain/model"
 	"lmm/api/context/blog/appservice"
-	"lmm/api/context/blog/domain/model"
 	"lmm/api/context/blog/domain/repository"
 	"lmm/api/http"
 	"log"
-	"fmt"
 )
 
 func PostBlog(c *http.Context) {
-	blog := model.Blog{}
+	blog := Blog{}
 	err := c.Request.ScanBody(&blog)
 	if err != nil {
 		http.BadRequest(c)
@@ -22,12 +21,12 @@ func PostBlog(c *http.Context) {
 	user := c.Values().Get("user").(*account.User)
 
 	app := appservice.New(repository.NewBlogRepository())
-	blogID, err := app.PostNewBlog(user.ID(), blog.Title(), blog.Text())
+	blogID, err := app.PostNewBlog(user.ID(), blog.Title, blog.Text)
 	if err != nil {
 		http.InternalServerError(c)
 		return
 	}
 
 	c.Header("Location", fmt.Sprintf("/blog/%d", blogID))
-	c.Status(http.StatusCreated)
+	c.Status(http.StatusCreated).String("success")
 }
