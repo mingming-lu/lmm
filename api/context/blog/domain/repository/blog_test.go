1ef30537c6c594f2fc7d16e2a5ac2c0f61202273
akinaru-lu <akinaru.lu@gmail.com>
2018-06-09 15:24:02 +0900

    补充blog仓储的测试用例

--- api/context/blog/domain/repository/blog_test.go
+++ api/context/blog/domain/repository/blog_test.go
@@ -32,7 +32,52 @@ func TestAddBlog_Success(tt *testing.T) {
 
 	stmt.QueryRow(blog.ID()).Scan(&userID, &blogTitle, &blogText)
 
-	tester.Is(user.ID(), userID)
-	tester.Is(blog.Title(), blogTitle)
-	tester.Is(blog.Text(), blogText)
+	t.Is(user.ID(), userID)
+	t.Is(blog.Title(), blogTitle)
+	t.Is(blog.Text(), blogText)
+}
+
+func TestAddBlog_DuplicateTitle(tt *testing.T) {
+	t := testing.NewTester(tt)
+	repo := NewBlogRepository()
+
+	name, password := uuid.New()[:31], uuid.New()
+	title, text := uuid.New(), uuid.New()
+	user, _ := accountFactory.NewUser(name, password)
+	blog, _ := factory.NewBlog(user.ID(), title, text)
+
+	err := repo.Add(blog)
+	t.NoError(err)
+
+	blogWithSameTitle, _ := factory.NewBlog(user.ID(), title, text)
+	err = repo.Add(blogWithSameTitle)
+	t.Regexp(`Duplicate entry '[\w\d-]+' for key 'title'`, err.Error())
+}
+
+func TestFindBlogByID_Success(tt *testing.T) {
+	t := testing.NewTester(tt)
+	repo := NewBlogRepository()
+
+	name, password := uuid.New()[:31], uuid.New()
+	title, text := uuid.New(), uuid.New()
+	user, _ := accountFactory.NewUser(name, password)
+	blog, _ := factory.NewBlog(user.ID(), title, text)
+	err := repo.Add(blog)
+
+	t.NoError(err)
+
+	blogFound, err := repo.FindByID(blog.ID())
+	t.NoError(err)
+	t.Is(blog.ID(), blogFound.ID())
+	t.Is(blog.Title(), blogFound.Title())
+	t.Is(blog.Text(), blogFound.Text())
+}
+
+func TestFindBlogByID_NotFound(tt *testing.T) {
+	t := testing.NewTester(tt)
+	repo := NewBlogRepository()
+
+	blog, err := repo.FindByID(uint64(777))
+	t.Error(err, "sql: no rows in result set")
+	t.Nil(blog)
 }
