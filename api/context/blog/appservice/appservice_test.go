400140392088bf858a1972dc1def103b102fcef6
akinaru-lu <akinaru.lu@gmail.com>
2018-06-09 13:55:37 +0900

    testing for blog appservice

---
+++ api/context/blog/appservice/appservice_test.go
@@ -0,0 +1,62 @@
+package appservice
+
+import (
+	accountFactory "lmm/api/context/account/domain/factory"
+	account "lmm/api/context/account/domain/model"
+	accountRepository "lmm/api/context/account/domain/repository"
+	"lmm/api/context/blog/domain/repository"
+	"lmm/api/testing"
+	"lmm/api/utils/uuid"
+	"os"
+)
+
+var user *account.User
+
+func TestMain(m *testing.M) {
+	name, password := uuid.New()[:32], uuid.New()
+	user, _ = accountFactory.NewUser(name, password)
+	accountRepository.New().Add(user)
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestPostNewBlog_Success(tt *testing.T) {
+	t := testing.NewTester(tt)
+	repo := repository.NewBlogRepository()
+	app := New(repo)
+
+	title, text := uuid.New(), uuid.New()
+	blogID, err := app.PostNewBlog(user.ID(), title, text)
+
+	t.NoError(err)
+	t.True(blogID > uint64(0))
+
+	db := repo.DB()
+	defer db.Close()
+
+	stmt := db.MustPrepare(`SELECT title, text FROM blog WHERE id = ?`)
+	defer stmt.Close()
+
+	var (
+		blogTitle string
+		blogText  string
+	)
+	err = stmt.QueryRow(blogID).Scan(&blogTitle, &blogText)
+	t.NoError(err)
+	t.Is(title, blogTitle)
+	t.Is(text, blogText)
+}
+
+func TestPostNewBlog_DuplicateTitle(tt *testing.T) {
+	t := testing.NewTester(tt)
+	repo := repository.NewBlogRepository()
+	app := New(repo)
+
+	title, text := uuid.New(), uuid.New()
+	_, err := app.PostNewBlog(user.ID(), title, text)
+	t.NoError(err)
+
+	_, err = app.PostNewBlog(user.ID(), title, text)
+	t.Is(ErrBlogTitleDuplicated, err)
+}
