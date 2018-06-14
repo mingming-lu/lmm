42145ea55aa4ed358b1b9db0430821baacad93b7
akinaru-lu <akinaru.lu@gmail.com>
2018-06-14 10:04:59 +0900

    behavior of category domain model

--- api/context/blog/domain/model/category.go
+++ api/context/blog/domain/model/category.go
@@ -4,5 +4,18 @@ import "lmm/api/domain/model"
 
 type Category struct {
 	model.Entity
+	id   uint64
 	name string
+}
+
+func (c *Category) ID() uint64 {
+	return c.id
+}
+
+func (c *Category) Name() string {
+	return c.name
+}
+
+func (c *Category) UpdateCategory(newName string) {
+	c.name = newName
 }
