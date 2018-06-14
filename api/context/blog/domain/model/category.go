9338eef0a6cf6b56f3b37e81dfe9cde2f3522226
akinaru-lu <akinaru.lu@gmail.com>
2018-06-14 10:17:17 +0900

    should validate name when create a new category

--- api/context/blog/domain/model/category.go
+++ api/context/blog/domain/model/category.go
@@ -1,11 +1,29 @@
 package model
 
-import "lmm/api/domain/model"
+import (
+	"errors"
+	"lmm/api/domain/model"
+)
+
+var (
+	ErrInvalidCategoryName = errors.New("invalid category name")
+)
 
 type Category struct {
 	model.Entity
 	id   uint64
 	name string
+}
+
+func NewCategory(id uint64, name string) (*Category, error) {
+	c := &Category{
+		id:   id,
+		name: name,
+	}
+	if c.validateName(c.name) != nil {
+		return nil, ErrInvalidCategoryName
+	}
+	return c, nil
 }
 
 func (c *Category) ID() uint64 {
@@ -16,6 +34,15 @@ func (c *Category) Name() string {
 	return c.name
 }
 
-func (c *Category) UpdateCategory(newName string) {
+func (c *Category) UpdateCategory(newName string) error {
+	if c.validateName(newName) != nil {
+		return ErrInvalidCategoryName
+	}
 	c.name = newName
+	return nil
+}
+
+func (c *Category) validateName(name string) error {
+	// TODO validating name by regexp
+	return nil
 }
