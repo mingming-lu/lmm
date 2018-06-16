073e3de8bb2c8e58365068bed86f2f8e9bfb6fa3
akinaru-lu <akinaru.lu@gmail.com>
2018-06-17 08:40:32 +0900

    omit hyphen from uuid.New

--- api/utils/strings/strings.go
+++ api/utils/strings/strings.go
@@ -2,6 +2,7 @@ package strings
 
 import (
 	"strconv"
+	"strings"
 )
 
 func Uint64ToStr(i uint64) string {
@@ -15,4 +16,8 @@ func StrToInt(s string) (int, error) {
 
 func StrToUint64(s string) (uint64, error) {
 	return strconv.ParseUint(s, 10, 64)
+}
+
+func ReplaceAll(s, old, new string) string {
+	return strings.Replace(s, old, new, -1)
 }
