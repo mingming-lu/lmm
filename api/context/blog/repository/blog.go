ba758c09745086cb4f3dcbc85338eb0a2964ef68
akinaru-lu <akinaru.lu@gmail.com>
2018-06-23 01:27:30 +0900

    repo: find all blog by category

--- api/context/blog/repository/blog.go
+++ api/context/blog/repository/blog.go
@@ -81,6 +81,50 @@ func (repo *blogRepo) FindAll(count, page int) ([]*model.Blog, error) {
 	return blogList, nil
 }
 
+func (repo *blogRepo) FindAllByCategory(category *model.Category, count, page uint) ([]*model.Blog, error) {
+	db := repo.DB()
+	defer db.Close()
+
+	stmt := db.MustPrepare(`
+		SELECT b.id, b.user, b.title, b.text, b.created_at, b.updated_at
+		FROM blog_category AS bc
+		INNER JOIN blog AS b ON b.id = bc.blog
+		WHERE bc.category = ?
+		ORDER BY b.created_at
+		LIMIT ?
+		OFFSET ?
+	`)
+	defer stmt.Close()
+
+	rows, err := stmt.Query(category.ID(), count+1, (page-1)*count)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	blogList := make([]*model.Blog, 0)
+	var (
+		blogID        uint64
+		blogWriter    uint64
+		blogTitle     string
+		blogText      string
+		blogCreatedAt time.Time
+		blogUpdatedAt time.Time
+	)
+
+	for rows.Next() {
+		if err := rows.Scan(
+			&blogID, &blogWriter, &blogTitle, &blogText, &blogCreatedAt, &blogUpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		blog := model.NewBlog(blogID, blogWriter, blogTitle, blogText, blogCreatedAt, blogUpdatedAt)
+		blogList = append(blogList, blog)
+	}
+
+	return blogList, nil
+}
+
 func (repo *blogRepo) FindByID(id uint64) (*model.Blog, error) {
 	db := repo.DB()
 	defer db.Close()
