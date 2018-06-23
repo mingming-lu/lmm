bcbfe01984d73d36a430c5c93b6c25709151f839
akinaru-lu <akinaru.lu@gmail.com>
2018-06-24 02:47:55 +0900

    blog_category change index

--- script/sql/20180622-alter-blog_category-modify-unique-key.sql
+++ script/sql/20180622-alter-blog_category-modify-unique-key.sql
@@ -4,4 +4,4 @@ ALTER TABLE `blog_category` ADD UNIQUE KEY `blog_category` (`blog`, `category`);
 -- re-alter by following sql
 ALTER TABLE `blog_category` DROP KEY `blog_category`;
 ALTER TABLE `blog_category` ADD UNIQUE KEY `blog` (`blog`);
-ALTER TABLE `blog_category` ADD INDEX `blog` (`blog`);
+ALTER TABLE `blog_category` ADD INDEX `category` (`category`, `blog`);
