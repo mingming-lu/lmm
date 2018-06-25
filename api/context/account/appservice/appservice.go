ac1268850c1dfb96da8386c58fe148844e46a7d6
Mingming Lu <akinaru.lu@gmail.com>
2018-06-25 20:20:56 +0900

    refactoring user context

--- api/context/account/appservice/appservice.go
+++ api/context/account/appservice/appservice.go
@@ -1,70 +1,76 @@
 package appservice
 
 import (
+	"encoding/json"
 	"errors"
-	"lmm/api/context/account/domain/factory"
+	"io"
 	"lmm/api/context/account/domain/model"
 	"lmm/api/context/account/domain/repository"
 	"lmm/api/context/account/domain/service"
 	"lmm/api/storage"
+	"regexp"
 )
 
 var (
 	ErrDuplicateUserName         = errors.New("User name duplicated")
 	ErrEmptyUserNameOrPassword   = errors.New("Empty user name or password")
+	ErrInvalidAuthorization      = errors.New("invalid authorization")
 	ErrInvalidInput              = errors.New("Invalid input")
 	ErrInvalidToken              = errors.New("Invalid token")
 	ErrInvalidUserNameOrPassword = errors.New("Invalid user name or password")
 )
 
+var (
+	PatternBearerAuthorization = regexp.MustCompile(`^Bearer (.+)$`)
+)
+
 type AppService struct {
-	repo repository.Repository
+	userService *service.UserService
 }
 
-func New(repo repository.Repository) *AppService {
-	return &AppService{repo: repo}
+type Auth struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
 }
 
-func (app *AppService) SignUp(name, password string) (uint64, error) {
-	if name == "" || password == "" {
-		return 0, ErrEmptyUserNameOrPassword
+func New(db *storage.DB) *AppService {
+	return &AppService{
+		userService: service.NewUserService(repository.New(db)),
 	}
+}
 
-	user, err := factory.NewUser(name, password)
+func (app *AppService) SignUp(requestBody io.ReadCloser) (uint64, error) {
+	auth := Auth{}
+	json.NewDecoder(requestBody).Decode(&auth)
+
+	user, err := app.userService.Register(auth.Name, auth.Password)
 	if err != nil {
-		return 0, err
-	}
-	err = app.repo.Add(user)
-	if err != nil {
-		key, _, ok := storage.CheckErrorDuplicate(err.Error())
-		if !ok {
-			return 0, err
-		}
-		if key == "name" {
-			return 0, ErrDuplicateUserName
-		}
 		return 0, err
 	}
 	return user.ID(), nil
 }
 
 // SignIn is a usecase which users sign in with a account
-func (app *AppService) SignIn(name, password string) (*model.User, error) {
-	if name == "" || password == "" {
-		return nil, ErrEmptyUserNameOrPassword
-	}
+func (app *AppService) SignIn(requestBody io.ReadCloser) (*model.User, error) {
+	auth := Auth{}
+	json.NewDecoder(requestBody).Decode(&auth)
 
-	user, err := app.repo.FindByName(name)
+	user, err := app.userService.Login(auth.Name, auth.Password)
+
+	// // user, err := app.repo.FindByName(name)
+	// if err != nil {
+	// 	if err.Error() == storage.ErrNoRows.Error() {
+	// 		return nil, ErrInvalidUserNameOrPassword
+	// 	}
+	// 	return nil, err
+	// }
 	if err != nil {
-		if err.Error() == storage.ErrNoRows.Error() {
-			return nil, ErrInvalidUserNameOrPassword
-		}
 		return nil, err
 	}
 
-	if user.VerifyPassword(password) != nil {
-		return nil, ErrInvalidUserNameOrPassword
-	}
+	// if user.VerifyPassword(password) != nil {
+	// 	return nil, ErrInvalidUserNameOrPassword
+	// }
 
 	return model.NewUser(
 		user.ID(),
@@ -76,14 +82,25 @@ func (app *AppService) SignIn(name, password string) (*model.User, error) {
 }
 
 func (app *AppService) VerifyToken(encodedToken string) (user *model.User, err error) {
-	token, err := service.DecodeToken(encodedToken)
-	if err != nil {
-		return nil, ErrInvalidToken
-	}
+	// token, err := service.DecodeToken(encodedToken)
+	// if err != nil {
+	// 	return nil, ErrInvalidToken
+	// }
 
-	user, err = app.repo.FindByToken(token)
-	if err != nil {
-		return nil, ErrInvalidToken
+	// user, err = app.repo.FindByToken(token)
+	// if err != nil {
+	// 	return nil, ErrInvalidToken
+	// }
+	// return user, nil
+	return nil, nil
+}
+
+func (app *AppService) BearerAuth(auth string) (*model.User, error) {
+	matched := PatternBearerAuthorization.FindStringSubmatch(auth)
+	if len(matched) != 2 {
+		return nil, ErrInvalidAuthorization
 	}
-	return user, nil
+	token := matched[1]
+
+	return app.userService.GetUserByHashedToken(token)
 }
---
+++ api/context/account/domain/service/user.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"lmm/api/context/account/domain/factory"
+	"lmm/api/context/account/domain/model"
+	"lmm/api/context/account/domain/repository"
+	"lmm/api/storage"
+
+	"github.com/akinaru-lu/errors"
+)
+
+var (
+	ErrDuplicateUserName = errors.New("User name duplicated")
+)
+
+type UserService struct {
+	repo repository.Repository
+}
+
+func NewUserService(repo repository.Repository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func (s *UserService) Register(name, password string) (*model.User, error) {
+	user, err := factory.NewUser(name, password)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.repo.Add(user); err != nil {
+		key, _, ok := storage.CheckErrorDuplicate(err.Error())
+		if !ok {
+			return nil, err
+		}
+		if key == "name" {
+			return nil, ErrDuplicateUserName
+		}
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *UserService) Login(name, password string) (*model.User, error) {
+	user, err := s.repo.FindByName(name)
+	if err != nil {
+		return nil, err
+	}
+	if err := user.VerifyPassword(password); err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *UserService) GetUserByHashedToken(hashedToken string) (*model.User, error) {
+	rawToken, err := DecodeToken(hashedToken)
+	if err != nil {
+		return nil, err
+	}
+	return s.repo.FindByToken(rawToken)
+}
