8608a1cf0b6d1cd85a33b57e5f994bafff404ece
akinaru-lu <akinaru.lu@gmail.com>
2018-06-28 00:55:48 +0900

    domain repository interface for blog tag

---
+++ api/context/blog/domain/repository/tag.go
@@ -0,0 +1,13 @@
+package repository
+
+import "lmm/api/context/blog/domain/model"
+
+type TagRepository interface {
+	Add(tag *model.Tag) error
+	FindByID(id uint64) (*model.Tag, error)
+	FindByName(name string) (*model.Tag, error)
+	FindAll() ([]*model.Tag, error)
+	FindAllByBlog(blog *model.Blog) ([]*model.Tag, error)
+	Update(tag *model.Tag) error
+	Remove(tag *model.Tag) error
+}
