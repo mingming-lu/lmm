262381ec2a00acb4e2864b9aee5ad766133bb094
akinaru-lu <akinaru.lu@gmail.com>
2018-06-29 03:08:46 +0900

    definitions of behaviors of tag model

--- api/context/blog/domain/model/tag.go
+++ api/context/blog/domain/model/tag.go
@@ -4,5 +4,35 @@ import "lmm/api/domain/model"
 
 type Tag struct {
 	model.Entity
-	name string
+	id   uint64
+	data TagData
+}
+
+type TagData struct {
+	model.ValueObject
+	blogID uint64
+	name   string
+}
+
+func NewTag(id, blogID uint64, name string) (*Tag, error) {
+	data := TagData{blogID: blogID, name: name}
+	return &Tag{id: id, data: data}, nil
+}
+
+func (t *Tag) ID() uint64 {
+	return t.id
+}
+
+func (t *Tag) BlogID() uint64 {
+	return t.data.blogID
+}
+
+func (t *Tag) Name() string {
+	return t.data.name
+}
+
+func (t *Tag) UpdateName(newName string) error {
+	// TODO validate name
+	t.data.name = newName
+	return nil
 }
