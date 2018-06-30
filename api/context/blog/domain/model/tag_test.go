8cd5c4bda42500ef65d8c4db768b4b61009edd60
akinaru-lu <akinaru.lu@gmail.com>
2018-07-01 01:21:40 +0900

    testing tag model

---
+++ api/context/blog/domain/model/tag_test.go
@@ -0,0 +1,33 @@
+package model
+
+import "lmm/api/testing"
+
+func TestNewTag_Success(tt *testing.T) {
+	t := testing.NewTester(tt)
+	tag, err := NewTag(123, 234, "tag name")
+	t.NoError(err)
+	t.Is(uint64(123), tag.ID())
+	t.Is(uint64(234), tag.BlogID())
+	t.Is("tag name", tag.Name())
+}
+
+func TestNewTag_InvalidName(tt *testing.T) {
+	t := testing.NewTester(tt)
+	tag, err := NewTag(123, 234, "@#?")
+	t.IsError(ErrInvalidTagName, err)
+	t.Nil(tag)
+}
+
+func TestUpdateTagName_Success(tt *testing.T) {
+	t := testing.NewTester(tt)
+	tag, err := NewTag(123, 234, "tag name")
+	t.NoError(err)
+	t.NoError(tag.UpdateName("new tag name"))
+}
+
+func TestUpdateTagName_InvalidName(tt *testing.T) {
+	t := testing.NewTester(tt)
+	tag, err := NewTag(333, 111, "tag name")
+	t.NoError(err)
+	t.IsError(ErrInvalidTagName, tag.UpdateName(""))
+}
