af3acb7e684ac7c2ac1e382126ffab8c605a33df
akinaru-lu <akinaru.lu@gmail.com>
2018-07-20 01:02:53 +0900

    feature: adding new tag to blog in application layer

---
+++ api/context/blog/appservice/tag.go
@@ -0,0 +1,23 @@
+package appservice
+
+import (
+	account "lmm/api/context/account/domain/model"
+	"lmm/api/context/blog/domain/factory"
+	"lmm/api/context/blog/domain/service"
+	"lmm/api/utils/strings"
+)
+
+func (app *AppService) AddNewTagToBlog(user *account.User, blogIDStr, tagName string) error {
+	blogID, err := strings.StrToUint64(blogIDStr)
+	if err != nil {
+		return service.ErrNoSuchBlog
+	}
+	tag, err := factory.NewTag(blogID, tagName)
+	if err != nil {
+		return err
+	}
+	if err := app.tagRepository.Add(tag); err != nil {
+		return err
+	}
+	return nil
+}
