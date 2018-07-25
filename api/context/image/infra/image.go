93d47fcb06382245ff015c431d6f7cd7b07f1b4e
akinaru-lu <akinaru.lu@gmail.com>
2018-07-26 00:43:22 +0900

    feature: implementation of image repository

---
+++ api/context/image/infra/image.go
@@ -0,0 +1,59 @@
+package infra
+
+import (
+	"lmm/api/context/image/domain"
+	"lmm/api/context/image/domain/model"
+	"lmm/api/storage"
+	"time"
+)
+
+type ImageStorage struct {
+	db *storage.DB
+}
+
+func NewImageStorage(db *storage.DB) *ImageStorage {
+	return &ImageStorage{db: db}
+}
+
+func (s *ImageStorage) Add(image *model.Image) error {
+	stmt := s.db.MustPrepare(`INSERT INTO image (uid, user, created_at) VALUES (?, ?, ?)`)
+	defer stmt.Close()
+
+	_, err := stmt.Exec(image.ID(), image.UserID(), image.CreatedAt())
+
+	if key, _, ok := storage.CheckErrorDuplicate(err); ok {
+		switch key {
+		case "uid":
+			return domain.ErrDuplicateImageID
+		}
+	}
+
+	return err
+}
+
+func (s *ImageStorage) Remove(image *model.Image) error {
+	stmt := s.db.MustPrepare(`DELETE FROM image WHERE uid = ?`)
+	defer stmt.Close()
+
+	_, err := stmt.Exec(image.ID())
+	return err
+}
+
+func (s *ImageStorage) FindByID(id string) (*model.Image, error) {
+	stmt := s.db.MustPrepare(`SELECT uid, user, created_at FROM image WHERE uid = ?`)
+	defer stmt.Close()
+
+	var (
+		imageID           string
+		imageUploaderID   uint64
+		imageUploaderTime time.Time
+	)
+	if err := stmt.QueryRow(id).Scan(&imageID, &imageUploaderID, &imageUploaderTime); err != nil {
+		switch err {
+		case storage.ErrNoRows:
+			return nil, domain.ErrNoSuchImage
+		}
+	}
+
+	return model.NewImage(imageID, imageUploaderID, imageUploaderTime), nil
+}
