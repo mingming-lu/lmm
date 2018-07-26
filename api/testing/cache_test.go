99e09a321bc42f20ada86a0be6193640fe8e080a
Mingming Lu <akinaru.lu@gmail.com>
2018-07-26 12:53:59 +0900

    test: example of set/get struct

--- api/testing/cache_test.go
+++ api/testing/cache_test.go
@@ -32,3 +32,30 @@ func TestCacheConn_SetString(tt *T) {
 	t.NoError(err)
 	t.Is(s, sGot)
 }
+
+func TestCacheConn_ScanStruct(tt *T) {
+	t := NewTester(tt)
+	conn := cache.Get()
+	defer func() {
+		t.NoError(conn.Close())
+	}()
+
+	type Dummy struct {
+		Message string `redis:"message"`
+	}
+
+	dummy := Dummy{Message: "dummy message"}
+
+	_, err := conn.Do("HSET", redis.Args{}.Add("MYSTERIOUS_STRUCT").AddFlat(&dummy)...)
+	t.NoError(err)
+
+	reply, err := conn.Do("HGETALL", "MYSTERIOUS_STRUCT")
+	t.NoError(err)
+
+	values, err := redis.Values(reply, err)
+	t.NoError(err)
+
+	scanned := Dummy{}
+	t.NoError(redis.ScanStruct(values, &scanned))
+	t.Is(dummy, scanned)
+}
