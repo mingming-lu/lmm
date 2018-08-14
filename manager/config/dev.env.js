7e7822a499c55672e0a647e1a4b83de7b2928562
akinaru-lu <akinaru.lu@gmail.com>
2018-08-15 02:21:59 +0900

    fix: asset server host name changed

--- manager/config/dev.env.js
+++ manager/config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_URL_BASE: '"http://api.lmm.local"',
-  IMAGE_URL_BASE: '"http://image.lmm.local"'
+  IMAGE_URL_BASE: '"http://static.lmm.local/images"'
 })
