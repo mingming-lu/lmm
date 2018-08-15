a38fc4615b59ea6e01aba6174b225a22b460b574
Mingming Lu <akinaru.lu@gmail.com>
2018-08-15 15:10:13 +0900

    test: rand

--- api/utils/rand/rand_test.go
+++ api/utils/rand/rand_test.go
@@ -1,8 +1,11 @@
 package rand
 
-import "lmm/api/testing"
+import (
+	"lmm/api/testing"
+	"time"
+)
 
-func TestDefault_Success(tt *testing.T) {
+func TestDefault_Output(tt *testing.T) {
 	t := testing.NewTester(tt)
 	t.Log("=========base36=========")
 	t.Log(Base36(8))
@@ -21,4 +24,25 @@ func TestDefault_Success(tt *testing.T) {
 	t.Log(Base64(8))
 	t.Log(Base64(8))
 	t.Log()
+}
+
+func TestNew(tt *testing.T) {
+	t := testing.NewTester(tt)
+
+	r := New(0)
+
+	Seed(0)
+	defer Seed(time.Now().UnixNano())
+
+	t.Is(Base36(5), r.Base36(5))
+	t.Is(Base62(9), r.Base62(9))
+	t.Is(Base36(6), r.Base36(6))
+}
+
+func TestBase(tt *testing.T) {
+	t := testing.NewTester(tt)
+
+	b := base(defaultRand, 6, 62)
+	t.Is(6, len(b))
+	t.Is(6, cap(b))
 }
