1a46d0a74f2a0b96c5e7d73d16e4ed933464d2e2
akinaru-lu <akinaru.lu@gmail.com>
2018-08-19 03:01:19 +0900

    feat: generate id through repository interface

--- api/context/article/domain/repository/article.go
+++ api/context/article/domain/repository/article.go
@@ -3,6 +3,7 @@ package repository
 import "lmm/api/context/article/domain/model"
 
 type ArticleRepository interface {
+	NextID() string
 	Save(*model.Article) error
 	Update(*model.Article) error
 	Find(model.ArticleID) (*model.Article, error)
