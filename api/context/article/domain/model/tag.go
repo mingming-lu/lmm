77d64c6ebea1cf37876aa5d49b860469be72461f
akinaru-lu <akinaru.lu@gmail.com>
2018-09-17 17:33:10 +0900

    rerfactor: rename etc.

--- api/context/article/domain/model/tag.go
+++ api/context/article/domain/model/tag.go
@@ -1,54 +1,62 @@
 package model
 
 import (
+	"regexp"
+
 	"lmm/api/context/article/domain"
 	"lmm/api/domain/model"
 	"lmm/api/utils/strings"
-
-	"regexp"
 )
 
 var (
 	patternTagName = regexp.MustCompile("^[\u4e00-\u9fa5ぁ-んァ-ンa-zA-Z0-9-_ ]$")
 )
 
+// TagID is a value object to identify tag
 type TagID struct {
 	model.ValueObject
-	articleID ArticleID
+	articleID *ArticleID
 	name      string
 }
 
-func (id TagID) ArticleID() ArticleID {
+// ArticleID returns the linked article's id
+func (id TagID) ArticleID() *ArticleID {
 	return id.articleID
 }
 
+// Name returns the tag's name
 func (id TagID) Name() string {
 	return id.name
 }
 
-func (id TagID) Equals(anotherID TagID) bool {
-	return (id.ArticleID() == anotherID.ArticleID()) && (id.Name() == anotherID.Name())
+// Equals compares tag id with another
+func (id TagID) Equals(another *TagID) bool {
+	return (id.ArticleID() == another.ArticleID()) && (id.Name() == another.Name())
 }
 
+// Tag is the tag model
 type Tag struct {
 	model.Entity
-	id TagID
+	id *TagID
 }
 
-func NewTag(articleID ArticleID, name string) (*Tag, error) {
+// NewTag creates a new tag
+func NewTag(articleID *ArticleID, name string) (*Tag, error) {
 	name, err := validateTagName(name)
 	if err != nil {
 		return nil, err
 	}
 
-	id := TagID{articleID: articleID, name: name}
+	id := &TagID{articleID: articleID, name: name}
 	return &Tag{id: id}, nil
 }
 
-func (tag *Tag) ID() TagID {
+// ID returns the tag's id
+func (tag *Tag) ID() *TagID {
 	return tag.id
 }
 
+// Equals compares tag with another
 func (tag *Tag) Equals(anotherTag *Tag) bool {
 	return tag.ID().Equals(anotherTag.ID())
 }
--- api/context/base/domain/model/entity.go
+++ api/context/base/domain/model/entity.go
@@ -1,4 +1,5 @@
 package model
 
+// Entity is a model which can be identified by its id
 type Entity interface {
 }
