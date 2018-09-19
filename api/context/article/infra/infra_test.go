289451a280ca083f033d29a0ba47cd647f29c37c
akinaru-lu <akinaru.lu@gmail.com>
2018-09-20 06:37:24 +0900

    refactor: chagne schema

---
+++ api/context/article/infra/infra_test.go
@@ -0,0 +1,22 @@
+package infra
+
+import (
+	"os"
+
+	"lmm/api/storage"
+	"lmm/api/testing"
+)
+
+var (
+	articleStorage *ArticleStorage
+)
+
+func TestMain(m *testing.M) {
+	db := storage.NewDB()
+	defer db.CloseNow()
+
+	articleStorage = NewArticleStorage(db)
+
+	code := m.Run()
+	os.Exit(code)
+}
