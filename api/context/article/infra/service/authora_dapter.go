34bf85c7f14525495bd01ea2d90f10e211145ec5
akinaru-lu <akinaru.lu@gmail.com>
2018-09-20 09:32:49 +0900

    feat: implementing author service

---
+++ api/context/article/infra/service/authora_dapter.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"lmm/api/context/article/domain/model"
+	"lmm/api/storage"
+)
+
+// AuthorAdapter is an implementation of AuthorService
+type AuthorAdapter struct {
+	db *storage.DB
+}
+
+// NewAuthorAdapter is a construct of AuthorAdapter
+func NewAuthorAdapter(db *storage.DB) *AuthorAdapter {
+	return &AuthorAdapter{db: db}
+}
+
+// AuthorFromUserID implements AuthorAdapter.AuthorFromUserID
+func (a *AuthorAdapter) AuthorFromUserID(userID uint64) (*model.Author, error) {
+	stmt := a.db.MustPrepare(`SELECT id, name FROM user WHERE id = ?`)
+	defer stmt.Close()
+
+	var (
+		authorID   uint64
+		authorName string
+	)
+	if err := stmt.QueryRow(userID).Scan(&authorID, &authorName); err != nil {
+		return nil, err
+	}
+	return model.NewAuthor(int64(authorID), authorName), nil
+}
