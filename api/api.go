7fd367191724287954a66a3510eada7dc7fcb34b
akinaru-lu <akinaru.lu@gmail.com>
2018-09-26 01:24:15 +0900

    feat: move blog to article

--- api/api.go
+++ api/api.go
@@ -7,8 +7,6 @@ import (
 	articlePersistence "lmm/api/context/article/infra/persistence"
 	articleService "lmm/api/context/article/infra/service"
 	article "lmm/api/context/article/ui"
-	blogInfra "lmm/api/context/blog/infra"
-	blog "lmm/api/context/blog/ui"
 	imageInfra "lmm/api/context/image/infra"
 	img "lmm/api/context/image/ui"
 	"lmm/api/storage/static"
@@ -21,10 +19,10 @@ func NewRouter(db *storage.DB, cache *storage.Cache) *http.Router {
 	userRepo := accountInfra.NewUserStorage(db)
 	accountUI := account.New(userRepo)
 
-	blogRepo := blogInfra.NewBlogStorage(db)
-	categoryRepo := blogInfra.NewCategoryStorage(db)
-	tagRepo := blogInfra.NewTagStorage(db)
-	blogUI := blog.New(blogRepo, categoryRepo, tagRepo)
+	// blogRepo := blogInfra.NewBlogStorage(db)
+	// categoryRepo := blogInfra.NewCategoryStorage(db)
+	// tagRepo := blogInfra.NewTagStorage(db)
+	// blogUI := blog.New(blogRepo, categoryRepo, tagRepo)
 
 	imgRepo := imageInfra.NewImageStorage(db)
 	imgRepo.SetStaticRepository(static.NewLocalStaticRepository())
@@ -50,28 +48,28 @@ func NewRouter(db *storage.DB, cache *storage.Cache) *http.Router {
 	router.GET("/v1/articles/:articleID", articleUI.GetArticle)
 	router.GET("/v1/articleTags", articleUI.GetAllArticleTags)
 
-	// blog
-	router.GET("/v1/blog", blogUI.GetAllBlog)
-	router.GET("/v1/blog/:blog", blogUI.GetBlog)
-	router.POST("/v1/blog", accountUI.BearerAuth(blogUI.PostBlog))
-	router.PUT("/v1/blog/:blog", accountUI.BearerAuth(blogUI.UpdateBlog))
-	// blog category
-	router.GET("/v1/blog/:blog/category", blogUI.GetBlogCagetory)
-	router.PUT("/v1/blog/:blog/category", accountUI.BearerAuth(blogUI.SetBlogCategory))
-	// blog tag
-	router.GET("/v1/blog/:blog/tags", blogUI.GetAllTagsOfBlog)
-	router.POST("/v1/blog/:blog/tags", accountUI.BearerAuth(blogUI.NewBlogTag))
+	// // blog
+	// router.GET("/v1/blog", blogUI.GetAllBlog)
+	// router.GET("/v1/blog/:blog", blogUI.GetBlog)
+	// router.POST("/v1/blog", accountUI.BearerAuth(blogUI.PostBlog))
+	// router.PUT("/v1/blog/:blog", accountUI.BearerAuth(blogUI.UpdateBlog))
+	// // blog category
+	// router.GET("/v1/blog/:blog/category", blogUI.GetBlogCagetory)
+	// router.PUT("/v1/blog/:blog/category", accountUI.BearerAuth(blogUI.SetBlogCategory))
+	// // blog tag
+	// router.GET("/v1/blog/:blog/tags", blogUI.GetAllTagsOfBlog)
+	// router.POST("/v1/blog/:blog/tags", accountUI.BearerAuth(blogUI.NewBlogTag))
 
-	// category
-	router.GET("/v1/categories", blogUI.GetAllCategoris)
-	router.POST("/v1/categories", accountUI.BearerAuth(blogUI.PostCategory))
-	router.PUT("/v1/categories/:category", accountUI.BearerAuth(blogUI.UpdateCategory))
-	router.DELETE("/v1/categories/:category", accountUI.BearerAuth(blogUI.DeleteCategory))
+	// // category
+	// router.GET("/v1/categories", blogUI.GetAllCategoris)
+	// router.POST("/v1/categories", accountUI.BearerAuth(blogUI.PostCategory))
+	// router.PUT("/v1/categories/:category", accountUI.BearerAuth(blogUI.UpdateCategory))
+	// router.DELETE("/v1/categories/:category", accountUI.BearerAuth(blogUI.DeleteCategory))
 
-	// tag
-	router.GET("/v1/tags", blogUI.GetAllTags)
-	router.PUT("/v1/tags/:tag", accountUI.BearerAuth(blogUI.UpdateTag))
-	router.DELETE("/v1/tags/:tag", accountUI.BearerAuth(blogUI.DeleteTag))
+	// // tag
+	// router.GET("/v1/tags", blogUI.GetAllTags)
+	// router.PUT("/v1/tags/:tag", accountUI.BearerAuth(blogUI.UpdateTag))
+	// router.DELETE("/v1/tags/:tag", accountUI.BearerAuth(blogUI.DeleteTag))
 
 	// image
 	router.POST("/v1/images", accountUI.BearerAuth(imageUI.Upload))
