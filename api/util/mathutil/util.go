196440954c921279c9915ba84913cf3d4dd8c29c
akinaru-lu <akinaru.lu@gmail.com>
2018-09-30 00:51:04 +0900

    feat: add MaxInt

--- api/util/mathutil/util.go
+++ api/util/mathutil/util.go
@@ -4,6 +4,23 @@ import (
 	"math"
 )
 
+// MaxInt finds the maximum interger from geven integers
+// returns 0 if no integer passed
+func MaxInt(ints ...int) int {
+	if len(ints) == 0 {
+		return 0
+	}
+
+	max := math.MinInt64
+	for _, i := range ints {
+		if i > max {
+			max = i
+		}
+	}
+
+	return max
+}
+
 // MinInt finds the minimum integer from given integers
 // returns 0 if no integer passed
 func MinInt(ints ...int) int {
@@ -11,7 +28,7 @@ func MinInt(ints ...int) int {
 		return 0
 	}
 
-	min := math.MaxInt32
+	min := math.MaxInt64
 	for _, i := range ints {
 		if i < min {
 			min = i
