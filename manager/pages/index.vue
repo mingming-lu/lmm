9c6129a9064ee56b6fd3c288be303d0dc3a23cda
Mingming Lu <akinaru.lu@gmail.com>
2018-10-10 03:10:04 +0900

    feat: make link

--- manager/pages/index.vue
+++ manager/pages/index.vue
@@ -2,9 +2,9 @@
   <v-layout column>
     <h2>Management of <a href="http://lumingming.net" target="_blank">lmm</a>'s content</h2>
     <ul>
-      <li>Articles</li>
-      <li>Assets (Images)</li>
-      <li>Assets (Photos)</li>
+      <li><nuxt-link to="/articles">Articles</nuxt-link></li>
+      <li><nuxt-link to="/assets/images">Assets (Images)</nuxt-link></li>
+      <li><nuxt-link to="/assets/photos">Assets (Photos)</nuxt-link></li>
     </ul>
   </v-layout>
 </template>
