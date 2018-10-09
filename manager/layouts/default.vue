e028f2f16feb04f420d3d639863c2a2137d40523
Mingming Lu <akinaru.lu@gmail.com>
2018-10-10 02:29:48 +0900

    feat: base page

--- manager/layouts/default.vue
+++ manager/layouts/default.vue
@@ -30,77 +30,43 @@
       app
     >
       <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
-      <v-container>
+      <v-container class="container">
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :fixed="fixed"
       app
     >
-      <span>&copy; 2017</span>
+      <span>&copy; 2018 Lu Mingming</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: true,
+      items: [
+        { icon: 'home', title: 'Home', to: '/' },
+        { icon: 'create', title: 'Articles', to: '/articles' },
+        { icon: 'photo_library', title: 'Assets (Images)', to: '/assets/images' },
+        { icon: 'photo_camera', title: 'Assets (Photos)', to: '/assets/photos' }
+      ],
+      miniVariant: true,
+      title: 'Manager'
     }
   }
+}
 </script>
+<style scoped>
+.container {
+  font-size: 1.5em;
+}
+</style>
