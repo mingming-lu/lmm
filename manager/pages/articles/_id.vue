692c7dadafa6085d7a24f6544080285dadf24774
Mingming Lu <akinaru.lu@gmail.com>
2018-10-11 15:00:39 +0900

    feat: article edit page

--- manager/pages/articles/_id.vue
+++ manager/pages/articles/_id.vue
@@ -1,12 +1,79 @@
 <template>
-  <v-layout>
-  </v-layout>
+  <v-container grid-list-xl>
+    <v-layout :row="row" :column="!row">
+      <v-flex xs6>
+        <v-text-field label="title" required v-model="articleTitle"/>
+        <v-combobox
+          v-model="articleTags"
+          :items="tags"
+          label="tags"
+          chips
+          clearable
+          multiple
+        >
+          <template slot="selection" slot-scope="data">
+            <v-chip
+              close
+              @input="removeTag(data.item)"
+            >
+              <strong>{{ data.item }}</strong>&nbsp;
+            </v-chip>
+          </template>
+        </v-combobox>
+        <v-textarea label="body" required v-model="articleBody"/>
+      </v-flex>
+      <v-flex xs6>
+        <v-text-field readonly outline label="title preview" v-model="articleTitle"/>
+        <v-textarea   readonly outline label="body preview"  v-model="articleBody"/>
+      </v-flex>
+    </v-layout>
+    <v-btn
+      absolute
+      color="accent"
+      dark
+      fab
+      right
+      top
+    >
+      <v-icon>autorenew</v-icon>
+    </v-btn>
+  </v-container>
 </template>
 
 <script>
 export default {
+  validate({params}) {
+    return /^[\d\w]{8}$/.test(params.id)
+  },
+  asyncData({$axios, params}) {
+    return Promise.all([
+      $axios.get(`/v1/articles/${params.id}`),
+      $axios.get(`/v1/articleTags`)
+    ]).then(([article, tags]) => {
+      return {
+        articleTitle: article.data.title,
+        articleBody:  article.data.body,
+        articleTags:  article.data.tags.map(tag => { return tag.name }),
+        row:          false,
+        tags:         tags.data.map(tag => { return tag.name })
+      }
+    })
+  },
+  mounted() {
+    this.onResize()
+    window.addEventListener('resize', this.onResize, { passive: true })
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize, { passive: true })
+  },
+  methods: {
+    removeTag(item) {
+      this.articleTags.splice(this.articleTags.indexOf(item), 1)
+      this.articleTags = [...this.articleTags]
+    },
+    onResize() {
+      this.row = window.innerWidth > 960
+    }
+  }
 }
 </script>
-
-<style scoped>
-</style>
