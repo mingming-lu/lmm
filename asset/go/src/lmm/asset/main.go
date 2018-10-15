7b389349c086ea34c7edea1c877e872dbae79aa3
Mingming Lu <akinaru.lu@gmail.com>
2018-10-15 17:58:53 +0900

    refactor: docker-compose file for asset service

--- asset/go/src/lmm/asset/main.go
+++ asset/go/src/lmm/asset/main.go
@@ -12,6 +12,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.DefaultServeMux.HandleFunc("/", index)
 
+	log.Println("listening and serving at :8003")
 	if err := http.ListenAndServe(":8003", nil); err != nil {
 		log.Fatal(err.Error())
 	}
