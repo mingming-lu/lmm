85c01876fb799d04fda55d25610dbbda322bad77
akinaru-lu <akinaru.lu@gmail.com>
2018-10-20 03:05:10 +0900

    subscriber inside asset

---
+++ asset/go/src/lmm/subscriber/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@host.docker.internal:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"lmm_photo_uploaded", // name
+		false,                // durable
+		false,                // delete when unused
+		false,                // exclusive
+		false,                // no-wait
+		nil,                  // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for msg := range msgs {
+			log.Printf("Received a message: %s", msg.MessageId)
+			err := upload(msg.MessageId, msg.Body)
+			if err != nil {
+				log.Println(err.Error())
+			}
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func upload(name string, data []byte) error {
+	file, err := os.OpenFile("/static/photos/"+name, os.O_RDWR|os.O_CREATE|os.O_EXCL, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+
+	if _, err := w.Write(data); err != nil {
+		return err
+	}
+
+	return w.Flush()
+}
