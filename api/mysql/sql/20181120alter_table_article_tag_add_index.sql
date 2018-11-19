1a8862fcc1650b6fc4ae0f154d08e09f950c1625
akinaru-lu <akinaru.lu@gmail.com>
2018-11-20 01:02:32 +0900

    delete alter sql

--- api/mysql/sql/20181120alter_table_article_tag_add_index.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `article_tag` DROP INDEX `article_name`;
-ALTER TABLE `article_tag` ADD INDEX `name` (`name`);
-ALTER TABLE `article_tag` ADD INDEX `article_tag` (`article`, `sort`, `name`);
