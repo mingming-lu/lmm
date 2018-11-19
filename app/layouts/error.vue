0822b006bc549e2f9015bce9bb85296f1703b29d
akinaru-lu <akinaru.lu@gmail.com>
2018-11-20 05:03:36 +0900

    customize error page

---
+++ app/layouts/error.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="center container">
+    <p>{{statusCode}} {{message}}</p>
+    <nuxt-link
+      to="/"
+      class="link"
+      >
+      Home page
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    statusCode: {
+      type: Number,
+      default: 404,
+    },
+    message: {
+      type: String,
+      default: 'Page Not Found',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/styles.scss';
+.center {
+  position: relative;
+}
+.link{
+  text-decoration: underline;
+}
+</style>
