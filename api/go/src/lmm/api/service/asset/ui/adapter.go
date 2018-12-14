5e25ca6124539868aa80722b494a350d40a291b4
Mingming Lu <akinaru.lu@gmail.com>
2018-12-14 18:31:26 +0900

    content-type to extention adapter

--- api/go/src/lmm/api/service/asset/ui/adapter.go
+++ api/go/src/lmm/api/service/asset/ui/adapter.go
@@ -1,6 +1,16 @@
 package ui
 
-import "lmm/api/service/asset/domain/model"
+import (
+	"strings"
+
+	"lmm/api/service/asset/domain/model"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	errUnsupportContentType = errors.New("unsupport content type")
+)
 
 type imageListItem struct {
 	Name string `json:"name"`
@@ -9,6 +19,19 @@ type imageListItem struct {
 type imageListJSON struct {
 	Images      []imageListItem `json:"images"`
 	HasNextPage bool            `json:"hasNextPage"`
+}
+
+func contentTypeToExtention(contentType string) (string, error) {
+	switch strings.ToLower(contentType) {
+	case "image/png":
+		return "png", nil
+	case "image/jpeg", "image/jpg":
+		return "jpeg", nil
+	case "image/gif":
+		return "gif", nil
+	default:
+		return "", errors.Wrap(errUnsupportContentType, contentType)
+	}
 }
 
 func imageCollectionToJSON(collection *model.ImageCollection) *imageListJSON {
