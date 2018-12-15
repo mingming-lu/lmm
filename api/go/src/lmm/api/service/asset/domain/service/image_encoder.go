3cbdf92de8dda07054c17aa12544f700946db252
akinaru-lu <akinaru.lu@gmail.com>
2018-12-16 02:53:30 +0900

    do not convert to jpeg by default

--- api/go/src/lmm/api/service/asset/domain/service/image_encoder.go
+++ api/go/src/lmm/api/service/asset/domain/service/image_encoder.go
@@ -3,22 +3,38 @@ package service
 import (
 	"bytes"
 	"context"
-	"image"
-	"image/jpeg"
-	"lmm/api/service/asset/domain/model"
+	"mime/multipart"
+	"net/http"
+	"regexp"
 	"sync"
+
+	"github.com/pkg/errors"
+
+	"lmm/api/service/asset/domain"
+	"lmm/api/service/asset/domain/model"
 )
 
 var (
+	// DefaultImageEncoder implements ImageEncoder as default
 	DefaultImageEncoder ImageEncoder
+)
+
+var (
+	// ErrEmptyImage occurs when read 0 byte from image file
+	ErrEmptyImage = errors.New("empty image")
+
+	// ErrMemoryOverflows error
+	ErrMemoryOverflows = errors.New("memory overflows")
 )
 
 func init() {
 	DefaultImageEncoder = &imageEncoder{}
 }
 
+// ImageEncoder interface
 type ImageEncoder interface {
-	Encode(c context.Context, src image.Image) (model.Data, error)
+	// Encode encodes multipart.File into []byte followed by its extention
+	Encode(c context.Context, file multipart.File) (bytes model.Data, extention string, err error)
 }
 
 var bufPool = sync.Pool{
@@ -29,16 +45,64 @@ var bufPool = sync.Pool{
 
 type imageEncoder struct{}
 
-func (e *imageEncoder) Encode(ctx context.Context, src image.Image) (model.Data, error) {
+func (e *imageEncoder) Encode(ctx context.Context, file multipart.File) (data model.Data, ext string, err error) {
+	defer func() {
+		e := recover()
+		if e == nil {
+			return
+		}
+		panicErr, ok := e.(error)
+		if !ok {
+			panic(e)
+		}
+		switch panicErr {
+		case bytes.ErrTooLarge: // panicked by bytes.Buffer.ReadFrom
+			err = ErrMemoryOverflows
+		default:
+			panic(e)
+		}
+	}()
+
 	buf := bufPool.Get().(*bytes.Buffer)
 	defer bufPool.Put(buf)
 	buf.Reset()
 
-	if err := jpeg.Encode(buf, src, &jpeg.Options{Quality: 80}); err != nil {
-		return nil, err
+	var n int64
+	n, err = buf.ReadFrom(file)
+	if err != nil {
+		return
+	}
+	if n == 0 {
+		err = ErrEmptyImage
+		return
 	}
 
-	b := buf.Bytes()[:]
+	data = buf.Bytes()
+	ext, err = assignImageExtension(data)
 
-	return b, nil
+	return
+}
+
+var contentTypePattern = regexp.MustCompile(`^(.+)\/(.+)$`)
+
+func assignImageExtension(data []byte) (string, error) {
+	ct := http.DetectContentType(data)
+	switch ct {
+	case "image/bmp":
+		return "bmp", nil
+	case "image/gif":
+		return "gif", nil
+	case "image/jpeg":
+		return "jpeg", nil
+	case "image/png":
+		return "png", nil
+	case "image/webp":
+		return "webp", nil
+	default:
+		ss := contentTypePattern.FindStringSubmatch(ct)
+		if len(ss) != 3 {
+			return ct, domain.ErrUnsupportedImageFormat
+		}
+		return ss[2], domain.ErrUnsupportedImageFormat
+	}
 }
