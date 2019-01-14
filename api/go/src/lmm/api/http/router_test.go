854c3e41359058fabb6a5a21ef1dde2829389d48
akinaru-lu <akinaru.lu@gmail.com>
2019-01-15 00:58:56 +0900

    test (router not found handler)

---
+++ api/go/src/lmm/api/http/router_test.go
@@ -0,0 +1,48 @@
+package http
+
+import (
+	"lmm/api/testing"
+	"net/http"
+	"time"
+)
+
+func TestRouterNotFound(tt *testing.T) {
+	t := testing.NewTester(tt)
+
+	t.Run("DefaultNotFoundHandler", func(_ *testing.T) {
+		router := NewRouter()
+
+		req := testing.GET("/", nil)
+		res := testing.Do(req, router)
+
+		t.Is(StatusNotFound, res.StatusCode())
+		t.Is(StatusText(StatusNotFound), res.Body())
+	})
+
+	t.Run("SpecifiedNotFoundHandler", func(_ *testing.T) {
+		plainText := "pretended OK"
+
+		router := NewRouter()
+		router.NotFound(func(c Context) {
+			c.String(StatusOK, plainText)
+		})
+
+		req := testing.GET("/", nil)
+		res := testing.Do(req, router)
+
+		t.Is(StatusOK, res.StatusCode())
+		t.Is(plainText, res.Body())
+	})
+}
+
+func TestHandleTimeout(tt *testing.T) {
+	t := testing.NewTester(tt)
+
+	router := NewRouter()
+	router.GET("/timeout", func(c Context) {
+		time.Sleep(5 * time.Second)
+	})
+
+	res := testing.Do(testing.GET("/timeout", nil), router)
+	t.Is(http.StatusRequestTimeout, res.StatusCode())
+}
