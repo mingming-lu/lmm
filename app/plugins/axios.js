c6639ae9c4f85f5e85700d87b6ba7b79d0dd5795
akinaru-lu <akinaru.lu@gmail.com>
2019-01-24 04:45:04 +0900

    hot fix (no need to set HOST header in prod env)

--- app/plugins/axios.js
+++ app/plugins/axios.js
@@ -4,9 +4,10 @@ export default function ({ $axios, isDev }) {
   $axios.onRequest(config => {
     if (isDev) {
       config.httpsAgent = new https.Agent({ rejectUnauthorized: false })
-    }
-    if (process.server) {
-      config.headers.common['Host'] = process.env.API_HOST
+
+      if (process.server) {
+        config.headers.common['Host'] = process.env.API_HOST
+      }
     }
   })
 }
