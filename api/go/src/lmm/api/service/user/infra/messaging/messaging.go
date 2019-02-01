3b542f8e51cb457cab98e45c89f5764caa716fd1
akinaru-lu <akinaru.lu@gmail.com>
2019-02-02 03:45:28 +0900

    save user' changed role in transaction

--- api/go/src/lmm/api/service/user/infra/messaging/messaging.go
+++ api/go/src/lmm/api/service/user/infra/messaging/messaging.go
@@ -2,8 +2,10 @@ package messaging
 
 import (
 	"context"
+	"database/sql"
 
 	domain "lmm/api/domain/event"
+	"lmm/api/http"
 	"lmm/api/service/user/domain/event"
 	"lmm/api/storage/db"
 
@@ -27,16 +29,40 @@ func (s *Subscriber) OnUserRoleChanged(c context.Context, e domain.Event) error 
 		return errors.Wrap(domain.ErrInvalidEvent, e.Topic())
 	}
 
-	searchUsers := s.db.Prepare(c, `
-		select id, role from user where name in (?, ?) order by filed (name, ?, ?)
+	tx, err := s.db.Begin(c, &sql.TxOptions{
+		Isolation: sql.LevelRepeatableRead,
+	})
+	if err != nil {
+		return err
+	}
+
+	searchUsers, err := tx.Prepare(c, `
+		select id, role from user where name in (?, ?) order by field (name, ?, ?) for update
 	`)
+	if err != nil {
+		tx.Rollback()
+		http.Log().Panic(c, err.Error())
+	}
 	defer searchUsers.Close()
 
-	recordChangeHistory := s.db.Prepare(c, `
-		insert into user_role_change_history
-		operator, operator_role, target_user, from, to, changed_at
-		values (?, ?, ?, ?, ?, ?)
+	setRole, err := tx.Prepare(c, `
+		update user set role = ? where name = ?
 	`)
+	if err != nil {
+		tx.Rollback()
+		http.Log().Panic(c, err.Error())
+	}
+	defer setRole.Close()
+
+	recordChangeHistory, err := tx.Prepare(c, `
+		insert into user_role_change_history (
+			operator, operator_role, target_user, from_role, to_role, changed_at
+		) values (?, ?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		tx.Rollback()
+		http.Log().Panic(c, err.Error())
+	}
 	defer recordChangeHistory.Close()
 
 	var (
@@ -52,18 +78,29 @@ func (s *Subscriber) OnUserRoleChanged(c context.Context, e domain.Event) error 
 			userRoleChanged.OperatorUser(), userRoleChanged.TargetUser(),
 		)
 		if err != nil {
+			tx.Rollback()
 			return err
 		}
 
 		rows.Next()
 		if err := rows.Scan(&operatorUserID, &operatorUserRole); err != nil {
+			tx.Rollback()
 			return err
 		}
 		rows.Next()
 		if err := rows.Scan(&targetUserID, &targetUserRole); err != nil {
+			tx.Rollback()
 			return err
 		}
 		rows.Close()
+	}
+
+	{
+		_, err := setRole.Exec(c, userRoleChanged.TargetRole(), userRoleChanged.TargetUser())
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
 	}
 
 	{
@@ -76,9 +113,10 @@ func (s *Subscriber) OnUserRoleChanged(c context.Context, e domain.Event) error 
 			userRoleChanged.OccurredAt(),
 		)
 		if err != nil {
+			tx.Rollback()
 			return err
 		}
 	}
 
-	return nil
+	return tx.Commit()
 }
