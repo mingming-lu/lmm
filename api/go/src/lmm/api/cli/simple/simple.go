325477f620fad495ef1acfeb7ff3c172a0d7c7d5
akinaru-lu <akinaru.lu@gmail.com>
2019-02-02 00:11:55 +0900

    cli command to migration

--- api/go/src/lmm/api/cli/simple/simple.go
+++ api/go/src/lmm/api/cli/simple/simple.go
@@ -2,6 +2,7 @@ package simple
 
 import (
 	"context"
+	"lmm/api/storage/db"
 	"log"
 
 	"lmm/api/cli"
@@ -19,5 +20,31 @@ func init() {
 	cli.Register("hello-world", NewCommand(func(_ context.Context) error {
 		log.Println("hello world")
 		return nil
+	}))
+	cli.Register("addRoleColumnToUserTable", NewCommand(func(c context.Context) error {
+		mysql := db.DefaultMySQL()
+		defer mysql.Close()
+
+		_, err := mysql.Exec(c, `alter table user add column role VARCHAR(31) NOT NULL`)
+		return err
+	}))
+	cli.Register("createTableUserRoleChangeHistory", NewCommand(func(c context.Context) error {
+		mysql := db.DefaultMySQL()
+		defer mysql.Close()
+
+		_, err := mysql.Exec(c, `
+CREATE TABLE IF NOT EXISTS user_role_change_history (
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	operator BIGINT UNSIGNED NOT NULL, -- user.id
+	operator_role VARCHAR(31) NOT NULL, -- user.role
+	target_user BIGINT UNSIGNED NOT NULL, -- user.id
+	target_user_role VARCHAR(31) NOT NULL, -- user.role
+	target_role VARCHAR(31) NOT NULL, -- user.role
+	changed_at DATETIME NOT NULL,
+	PRIMARY KEY (id),
+	INDEX changed_at (changed_at)
+) ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
+`)
+		return err
 	}))
 }
