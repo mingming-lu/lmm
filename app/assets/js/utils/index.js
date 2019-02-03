a93950803ad96de499a91301aea9d96d0e5a8c33
akinaru-lu <akinaru.lu@gmail.com>
2019-02-04 00:54:48 +0900

    fix: months are always two digits

--- app/assets/js/utils/index.js
+++ app/assets/js/utils/index.js
@@ -7,19 +7,22 @@ export {
   range,
 }
 
-function formattedDateFromTimeStamp(timestamp) {
+const formattedDateFromTimeStamp = timestamp => {
   let date = new Date(timestamp * 1e3)
   return formattedDate(date)
 }
 
-function formattedDateFromString(s) {
+const formattedDateFromString = s => {
   let date = new Date(s)
   return formattedDate(date)
 }
 
-function formattedDate(date) {
+const formattedDate = date => {
   const y = date.getFullYear()
-  const m = date.getMonth() + 1
+  const m = (() => {
+    const _m = date.getMonth() + 1
+    return _m < 10 ? `0${_m}` : _m
+  })()
   const d = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
 
   return `${y}-${m}-${d} ${date.toTimeString().substr(0, 8)}`
