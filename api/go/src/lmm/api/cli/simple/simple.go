c92d4f4e5de37c3f6345f7371e422e0ee6439b47
akinaru-lu <akinaru.lu@gmail.com>
2019-02-12 23:48:17 +0900

    convert all table to utf8mb4

--- api/go/src/lmm/api/cli/simple/simple.go
+++ api/go/src/lmm/api/cli/simple/simple.go
@@ -2,9 +2,11 @@ package simple
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"lmm/api/cli"
+	"lmm/api/storage/db"
 )
 
 // NewCommand creates a cli.Command implement
@@ -18,6 +20,31 @@ func (exec NewCommand) Execute(c context.Context) error {
 func init() {
 	cli.Register("hello-world", NewCommand(func(_ context.Context) error {
 		log.Println("hello world")
+		return nil
+	}))
+
+	cli.Register("setCharacterSetTo-utf8mb4", NewCommand(func(c context.Context) error {
+		mysql := db.DefaultMySQL()
+		defer mysql.Close()
+
+		rows, err := mysql.Query(c, "SHOW TABLES")
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+
+		var table string
+
+		for rows.Next() {
+			if err := rows.Scan(&table); err != nil {
+				return err
+			}
+			_, err := mysql.Exec(c, fmt.Sprintf("ALTER TABLE %s CONVERT TO CHARACTER SET utf8mb4;", table))
+			if err != nil {
+				return err
+			}
+		}
+
 		return nil
 	}))
 }
