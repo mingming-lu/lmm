4347002b64088a48b75b8558470b598c4ce177aa
akinaru-lu <akinaru.lu@gmail.com>
2019-05-26 08:44:31 +0900

    test: user data store

--- api/go/src/lmm/api/service/user/infra/persistence/user_test.go
+++ api/go/src/lmm/api/service/user/infra/persistence/user_test.go
@@ -2,59 +2,127 @@ package persistence
 
 import (
 	"context"
+	"testing"
+	"time"
 
-	"github.com/google/uuid"
-	"github.com/pkg/errors"
-	"golang.org/x/crypto/bcrypt"
-
+	"lmm/api/clock"
+	"lmm/api/pkg/transaction"
 	"lmm/api/service/user/domain"
 	"lmm/api/service/user/domain/factory"
+	"lmm/api/service/user/domain/model"
 	"lmm/api/service/user/domain/service"
-	"lmm/api/testing"
-	"lmm/api/util/stringutil"
+	"lmm/api/util/uuidutil"
+
+	"cloud.google.com/go/datastore"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSaveUser(tt *testing.T) {
-	c := context.Background()
+func mustRandomUser(userDataStore *UserDataStore) *model.User {
+	username := "U" + uuidutil.NewUUID()[:8]
+	email := username + "@lmm.local"
+	password := uuidutil.NewUUID()
 
-	builder := factory.NewFactory(&service.BcryptService{})
+	f := factory.NewFactory(&service.BcryptService{}, userDataStore)
 
-	username := "U" + stringutil.ReplaceAll(uuid.New().String(), "-", "")[:8]
-	email := "admin@lmm.local"
-	password := "notweakpassword123!?"
-	user, err := builder.NewUser(username, email, password)
+	var user *model.User
+
+	err := userDataStore.RunInTransaction(context.Background(), func(tx transaction.Transaction) error {
+		userCreated, err := f.NewUser(tx, username, email, password)
+		user = userCreated
+		return err
+	}, nil)
 
 	if err != nil {
-		tt.Fatal(err)
+		panic("failed to create new user: " + err.Error())
 	}
 
-	tt.Run("Success", func(tt *testing.T) {
-		t := testing.NewTester(tt)
+	return user
+}
 
-		t.NoError(userRepo.Save(c, user))
+type testClock struct{}
 
-		var (
-			nameFound     string
-			emailFound    string
-			passwordFound string
-			tokenFound    string
-		)
-		t.NoError(
-			dbEngine.QueryRow(c,
-				`select name, email, password, token from user where name = ?`, username,
-			).Scan(&nameFound, &emailFound, &passwordFound, &tokenFound),
-		)
-		t.Is(username, nameFound)
-		t.Is(email, emailFound)
-		t.NoError(bcrypt.CompareHashAndPassword([]byte(passwordFound), []byte(password)))
+func (c testClock) Now() time.Time {
+	return time.Now().Truncate(1 * time.Second)
+}
+
+func TestUserDataStore(t *testing.T) {
+	c := context.Background()
+
+	clock.DefaultClock = &testClock{}
+
+	dataStore, err := datastore.NewClient(c, "")
+	if err != nil {
+		t.Fatal("failed to connect to datastore")
+	}
+	defer dataStore.Close()
+
+	userDataStore := NewUserDataStore(dataStore)
+
+	user := mustRandomUser(userDataStore)
+
+	t.Run("NextID", func(t *testing.T) {
+		userDataStore.RunInTransaction(c, func(tx transaction.Transaction) error {
+			userID, err := userDataStore.NextID(tx)
+			assert.NoError(t, err)
+			assert.NotZero(t, userID)
+			return nil
+		}, nil)
 	})
 
-	tt.Run("DuplicateUserName", func(tt *testing.T) {
-		t := testing.NewTester(tt)
+	t.Run("Save", func(t *testing.T) {
+		t.Run("Insert", func(t *testing.T) {
+			userDataStore.RunInTransaction(c, func(tx transaction.Transaction) error {
+				assert.NoError(t, userDataStore.Save(tx, user))
+				return nil
+			}, nil)
+		})
+	})
 
-		t.IsError(
-			domain.ErrUserNameAlreadyUsed,
-			errors.Cause(userRepo.Save(c, user)),
-		)
+	t.Run("FindByName", func(t *testing.T) {
+		t.Run("Found", func(t *testing.T) {
+			userDataStore.RunInTransaction(c, func(tx transaction.Transaction) error {
+				userFound, err := userDataStore.FindByName(tx, user.Name())
+				if !assert.NoError(t, err) {
+					t.Fatal(err)
+				}
+
+				assert.EqualValues(t, user, userFound)
+				return nil
+			}, nil)
+
+			t.Run("ReadOnly", func(t *testing.T) {
+				userDataStore.RunInTransaction(c, func(tx transaction.Transaction) error {
+					userFound, err := userDataStore.FindByName(tx, user.Name())
+					if !assert.NoError(t, err) {
+						t.Fatal(err)
+					}
+
+					assert.EqualValues(t, user, userFound)
+					return nil
+				}, &transaction.Option{ReadOnly: true})
+			})
+		})
+
+		t.Run("NotFound", func(t *testing.T) {
+			user := mustRandomUser(userDataStore)
+
+			userDataStore.RunInTransaction(c, func(tx transaction.Transaction) error {
+				userFound, err := userDataStore.FindByName(tx, user.Name())
+				assert.Error(t, domain.ErrNoSuchUser, err)
+				assert.Nil(t, userFound)
+
+				return nil
+			}, nil)
+
+			t.Run("ReadOnly", func(t *testing.T) {
+				userDataStore.RunInTransaction(c, func(tx transaction.Transaction) error {
+					userFound, err := userDataStore.FindByName(tx, user.Name())
+					assert.Error(t, domain.ErrNoSuchUser, err)
+					assert.Nil(t, userFound)
+
+					return nil
+				}, &transaction.Option{ReadOnly: true})
+			})
+		})
 	})
 }
