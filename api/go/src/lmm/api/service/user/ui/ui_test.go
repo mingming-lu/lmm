34160827004247e838f6250be9e70ae88cbe1e0b
Mingming Lu <akinaru.lu@gmail.com>
2019-05-27 15:21:46 +0900

    test POST /v1/users

--- api/go/src/lmm/api/service/user/ui/ui_test.go
+++ api/go/src/lmm/api/service/user/ui/ui_test.go
@@ -1,91 +1,131 @@
 package ui
 
-// func TestPostUser(tt *testing.T) {
-// 	username := "U" + stringutil.ReplaceAll(uuid.New().String(), "-", "")[:8]
-// 	email := username + "@lmm.local"
-// 	password := uuid.New().String()
-//
-// 	tt.Run("Success", func(tt *testing.T) {
-// 		t := testing.NewTester(tt)
-// 		res := postUser(testing.StructToRequestBody(signUpRequestBody{
-// 			Name:     username,
-// 			Email:    email,
-// 			Password: password,
-// 		}))
-//
-// 		t.Is(201, res.StatusCode())
-// 		t.Is("success", res.Body())
-// 	})
-//
-// 	tt.Run("Fail", func(tt *testing.T) {
-// 		cases := map[string]struct {
-// 			UserName   string
-// 			Email      string
-// 			Password   string
-// 			StatusCode int
-// 			Body       string
-// 		}{
-// 			"InvalidUserName": {
-// 				"1234", email, password, 400, domain.ErrInvalidUserName.Error(),
-// 			},
-// 			"DuplicateUserName": {
-// 				username, email, password, 409, domain.ErrUserNameAlreadyUsed.Error(),
-// 			},
-// 			"EmptyEmail": {
-// 				username, "", password, 400, domain.ErrInvalidEmail.Error(),
-// 			},
-// 			"InvalidEmail": {
-// 				username, "example.com", password, 400, domain.ErrInvalidEmail.Error(),
-// 			},
-// 			"EmptyPassword": {
-// 				username, email, "", 400, domain.ErrUserPasswordEmpty.Error(),
-// 			},
-// 			"InvalidPassword": {
-// 				username, email, "不合法的密码", 400, domain.ErrInvalidPassword.Error(),
-// 			},
-// 			"ShortPassword": {
-// 				username, email, "qwert", 400, domain.ErrUserPasswordTooShort.Error(),
-// 			},
-// 			"LongPassword": {
-// 				username, email, strings.Repeat("s", 251), 400, domain.ErrUserPasswordTooLong.Error(),
-// 			},
-// 			"WeakPassword": {
-// 				username, email, "password", 400, domain.ErrUserPasswordTooWeak.Error(),
-// 			},
-// 		}
-//
-// 		for testName, testCase := range cases {
-// 			tt.Run(testName, func(tt *testing.T) {
-// 				t := testing.NewTester(tt)
-// 				res := postUser(testing.StructToRequestBody(signUpRequestBody{
-// 					Name:     testCase.UserName,
-// 					Email:    testCase.Email,
-// 					Password: testCase.Password,
-// 				}))
-//
-// 				t.Is(testCase.StatusCode, res.StatusCode())
-// 				t.Is(testCase.Body, res.Body())
-// 			})
-// 		}
-// 	})
-// }
-//
-// func postUser(requestBody io.ReadCloser) *testing.Response {
-// 	request := testing.POST("/v1/users", &testing.RequestOptions{
-// 		FormData: requestBody,
-// 	})
-//
-// 	return testing.DoRequest(request, router)
-// }
-//
-// func getUsers(opts *testing.RequestOptions) *testing.Response {
-// 	req := testing.GET("/v1/users", opts)
-//
-// 	return testing.DoRequest(req, router)
-// }
-//
-// func assignUserRole(username string, opts *testing.RequestOptions) *testing.Response {
-// 	req := testing.PUT("/v1/users/"+username+"/role", opts)
-//
-// 	return testing.DoRequest(req, router)
-// }
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http/httptest"
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+
+	"lmm/api/http"
+	"lmm/api/service/user/application"
+	"lmm/api/service/user/domain"
+	"lmm/api/service/user/infra/persistence"
+	"lmm/api/service/user/infra/service"
+	"lmm/api/util/uuidutil"
+
+	"cloud.google.com/go/datastore"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	router *http.Router
+	ui     *UI
+)
+
+func TestMain(m *testing.M) {
+	dataStore, err := datastore.NewClient(context.Background(), "")
+	if err != nil {
+		panic(err)
+	}
+
+	userRepo := persistence.NewUserDataStore(dataStore)
+	userAppService := application.NewService(&service.BcryptService{}, userRepo, userRepo)
+	ui = NewUI(userAppService)
+
+	router = http.NewRouter()
+	router.POST("/v1/users", ui.SignUp)
+
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
+
+func TestPostV1Users(t *testing.T) {
+	username := "U" + uuidutil.NewUUID()[:8]
+	password := uuidutil.NewUUID() + uuidutil.NewUUID()
+	email := username + "@lmm.local"
+
+	t.Run("Created", func(t *testing.T) {
+		res := postV1Users(signUpRequestBody{
+			Name:     username,
+			Password: password,
+			Email:    email,
+		})
+
+		assert.Equal(t, 201, res.Code)
+		assert.Regexp(t, regexp.MustCompile(`users/\d+`), res.Header().Get("Location"))
+	})
+
+	t.Run("BadRequest", func(tt *testing.T) {
+		generateNewName := func() string {
+			return "U" + uuidutil.NewUUID()[:8]
+		}
+
+		cases := map[string]struct {
+			UserName   string
+			Email      string
+			Password   string
+			StatusCode int
+			Body       string
+		}{
+			"InvalidUserName": {
+				"1234", email, password, 400, domain.ErrInvalidUserName.Error(),
+			},
+			"DuplicateUserName": {
+				username, email, password, 409, domain.ErrUserNameAlreadyUsed.Error(),
+			},
+			"EmptyEmail": {
+				generateNewName(), "", password, 400, domain.ErrInvalidEmail.Error(),
+			},
+			"InvalidEmail": {
+				generateNewName(), "example.com", password, 400, domain.ErrInvalidEmail.Error(),
+			},
+			"EmptyPassword": {
+				generateNewName(), email, "", 400, domain.ErrUserPasswordEmpty.Error(),
+			},
+			"InvalidPassword": {
+				generateNewName(), email, "不合法的密码", 400, domain.ErrInvalidPassword.Error(),
+			},
+			"ShortPassword": {
+				generateNewName(), email, "qwert", 400, domain.ErrUserPasswordTooShort.Error(),
+			},
+			"LongPassword": {
+				generateNewName(), email, strings.Repeat("s", 251), 400, domain.ErrUserPasswordTooLong.Error(),
+			},
+			"WeakPassword": {
+				generateNewName(), email, "password", 400, domain.ErrUserPasswordTooWeak.Error(),
+			},
+		}
+
+		for testName, testCase := range cases {
+			t.Run(testName, func(tt *testing.T) {
+				res := postV1Users(signUpRequestBody{
+					Name:     testCase.UserName,
+					Email:    testCase.Email,
+					Password: testCase.Password,
+				})
+
+				assert.Equal(t, testCase.StatusCode, res.Code)
+				assert.Equal(t, testCase.Body, res.Body.String())
+			})
+		}
+	})
+}
+
+func postV1Users(body signUpRequestBody) *httptest.ResponseRecorder {
+	b, err := json.Marshal(body)
+	if err != nil {
+		panic(errors.Wrap(err, "failed to decode to json"))
+	}
+
+	req := httptest.NewRequest("POST", "/v1/users", bytes.NewReader(b))
+	res := httptest.NewRecorder()
+
+	router.ServeHTTP(res, req)
+
+	return res
+}
