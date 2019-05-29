8643472b40782cd44f051fd2343c8b08715f7a5f
akinaru-lu <akinaru.lu@gmail.com>
2019-05-30 01:11:40 +0900

    util to create test user

--- api/go/src/lmm/api/pkg/testing/user.go
+++ api/go/src/lmm/api/pkg/testing/user.go
@@ -1,16 +1,65 @@
 package testing
 
-import "cloud.google.com/go/datastore"
+import (
+	"context"
+	"time"
 
-// User is expected to be useful for testing
+	"lmm/api/service/user/domain/factory"
+	"lmm/api/service/user/infra/service"
+	"lmm/api/util/uuidutil"
+
+	"cloud.google.com/go/datastore"
+)
+
+var (
+	// TokenService uses CFBTokenService as default
+	TokenService = &service.CFBTokenService{}
+
+	// PasswordService uses BscryptService as default
+	PasswordService = &service.BcryptService{}
+)
+
+// User used for testing
 type User struct {
-	id       int64
-	name     string
-	email    string
-	password string
+	ID             int64     `datastore:"-"`
+	Name           string    `datastore:"Name"`
+	RawPassword    string    `datastore:"Password"`
+	HashedPassword string    `datastore:"-"`
+	RawToken       string    `datastore:"Token"`
+	AccessToken    string    `datastore:"-"`
+	RegisteredAt   time.Time `datastore:"RegisteredAt"`
 }
 
-// NewUser randomly create a new user
-func NewUser(source *datastore.Client) *User {
-	return &User{}
+// NewUser create a new user
+func NewUser(ctx context.Context, dataStore *datastore.Client) *User {
+	username := "U" + uuidutil.NewUUID()[:8]
+	password := uuidutil.NewUUID() + uuidutil.NewUUID()
+
+	hashedPassword, err := factory.NewFactory(PasswordService, nil).NewPassword(password)
+	if err != nil {
+		panic("failed to encrypt password: " + err.Error())
+	}
+
+	token := uuidutil.NewUUID()
+	accessToken, err := TokenService.Encrypt(token)
+	if err != nil {
+		panic("failed to generate access token: " + err.Error())
+	}
+
+	user := &User{
+		Name:           username,
+		RawPassword:    password,
+		HashedPassword: hashedPassword,
+		RawToken:       token,
+		AccessToken:    accessToken.Hashed(),
+		RegisteredAt:   time.Now(),
+	}
+	key, err := dataStore.Put(ctx, datastore.IncompleteKey("User", nil), user)
+	if err != nil {
+		panic("failed to put user: " + err.Error())
+	}
+
+	user.ID = key.ID
+
+	return user
 }
