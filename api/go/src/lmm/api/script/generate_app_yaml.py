7aa1a966d5c333ac10c562364f353edd167856c0
Mingming Lu <akinaru.lu@gmail.com>
2019-06-06 13:23:00 +0900

    fix: did not pass ASSET_BUCKET_NAME

--- api/go/src/lmm/api/script/generate_app_yaml.py
+++ api/go/src/lmm/api/script/generate_app_yaml.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--APP_ORIGIN')
     parser.add_argument('--MANAGER_ORIGIN')
+    parser.add_argument('--ASSET_BUCKET_NAME')
     parser.add_argument('--DATASTORE_PROJECT_ID')
     parser.add_argument('--GIN_MODE')
     parser.add_argument('--GOOGLE_APPLICATION_CREDENTIALS')
@@ -19,6 +20,7 @@ if __name__ == '__main__':
     yaml_file = template.format(
         APP_ORIGIN=args.APP_ORIGIN,
         MANAGER_ORIGIN=args.MANAGER_ORIGIN,
+        ASSET_BUCKET_NAME=args.ASSET_BUCKET_NAME,
         DATASTORE_PROJECT_ID=args.DATASTORE_PROJECT_ID,
         GIN_MODE=args.GIN_MODE,
         GOOGLE_APPLICATION_CREDENTIALS=args.GOOGLE_APPLICATION_CREDENTIALS,
