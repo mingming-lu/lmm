832ac0f9fbb184cf25ad066947e795ec66c96751
Mingming Lu <akinaru.lu@gmail.com>
2019-06-06 18:37:05 +0900

    fallback api url

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -52,7 +52,7 @@ module.exports = {
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
     baseURL: process.env.API_URL,
-    browserBaseURL: process.env.API_URL_BROWSER
+    browserBaseURL: process.env.API_URL_BROWSER || process.env.API_URL
   },
 
   /*
