1011775dc987115a7b14d350c01873be3d906acf
Lu Mingming <akinaru.lu@gmail.com>
2019-10-03 12:12:47 +0900

    feat: use goenv on CORS

--- api/go/src/lmm/api/pkg/http/middleware/cors.go
+++ api/go/src/lmm/api/pkg/http/middleware/cors.go
@@ -3,23 +3,23 @@ package middleware
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"regexp"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/proproto/goenv"
 )
 
 // CORS middleware
 func CORS(customDomain string) gin.HandlerFunc {
 	var re *regexp.Regexp
 	if customDomain == "" {
-		gaeProjectID := os.Getenv("GCP_PROJECT_ID")
-		if gaeProjectID == "" {
-			panic("GCP_PROJECT_ID needed")
-		}
-		pattern := fmt.Sprintf(`^https://(.+-dot-)*%s.appspot\.com$`, gaeProjectID)
+		config := struct {
+			ProjectID string `env:"GCP_PROJECT_ID,required"`
+		}{}
+		goenv.MustBind(&config)
+		pattern := fmt.Sprintf(`^https://(.+-dot-)*%s.appspot\.com$`, config.ProjectID)
 		re = regexp.MustCompile(pattern)
 	} else {
 		pattern := fmt.Sprintf(`^https://(.+\.)*%s$`, regexp.QuoteMeta(customDomain))
